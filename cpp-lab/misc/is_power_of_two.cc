0c44bf2db59fcd0b6acbb01eeb7b0ac21aba1f46
liubang <it.liubang@gmail.com>
2020-08-16 01:37:25 +0800

    update

---
+++ cpp-lab/misc/is_power_of_two.cc
@@ -0,0 +1,28 @@
+// 判断整数是不是2的幂次
+
+#include <iostream>
+#include <cstdint>
+
+/*
+bool is_power_of_two(uint64_t n) {
+  // 8      01000
+  // 12     01100
+  // 13     01101
+  // 15     01111
+  // 16     10000
+  //
+  // 不难发现，二进制只有1个bit为1的数字是2的幂次方
+
+  // 依赖gcc扩展
+  return __builtin_popcountll(n) == 1;
+}
+*/
+
+bool is_power_of_two(uint64_t n) {
+  // -n 实际上就是n按位取反然后加1的结果
+  // -n = ~n + 1
+  //  8     01000
+  // -8     10111 + 1
+  //        11000   // 消除了末尾的1，保留了原数字最后一位1
+  return (n != 0) && ((n & -n) == n);
+}
---
+++ cpp-lab/misc/producer.cc
@@ -0,0 +1,14 @@
+#include <memory>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+
+extern ::std::condition_variable cond;
+extern ::std::mutex mtx;
+extern ::std::queue<::std::shared_ptr<int>> queue;
+
+void produce(::std::shared_ptr<int>&& g) {
+  ::std::lock_guard<::std::mutex> lk(mtx);
+  queue.push(::std::move(g));
+  cond.notify_one();
+}
