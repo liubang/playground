b740b3efb19f38c4363a10b64c0e5d96c91332c3
liubang <it.liubang@gmail.com>
2020-11-04 11:24:52 +0800

    update

---
+++ cpp-lab/metaproggramming/maybe.cc
@@ -0,0 +1,85 @@
+#include <type_traits>
+#include <variant>
+#include <functional>
+#include <functional>
+#include <iostream>
+
+template <typename T>
+struct Just {
+  T value;
+  operator const T&() const {
+    return value;
+  }
+  Just(const T& value) : value(value) {}
+};
+
+struct Nothing {};
+
+template <typename T>
+struct Maybe {
+  std::variant<Just<T>, Nothing> value;
+  Maybe(const T& value) : value(value) {}
+  Maybe(const Nothing&) : value(Nothing{}) {}
+};
+
+template <class... Ts>
+struct overloaded : Ts... {
+  using Ts::operator()...;
+};
+
+template <class... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;
+
+template <typename T>
+auto just(T x) -> Maybe<T> {
+  return x;
+}
+
+struct AnyType {
+  template <typename T>
+  operator T() {
+    return T{};
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const AnyType&) {
+    return os;
+  }
+};
+
+auto nothing() -> Maybe<AnyType> {
+  return Nothing{};
+}
+
+template <typename T, typename F>
+auto operator>>=(const Maybe<T>& ma, F&& f) {
+  using R = std::invoke_result_t<F, T>;
+  return std::visit(
+      overloaded{
+          [&](const Just<T>& v) -> R { return f(static_cast<T>(v)); },
+          [](Nothing) -> R { return Nothing{}; }},
+      ma.value);
+}
+
+template <typename T>
+auto show(const Maybe<T>& value) -> void {
+  std::visit(
+      overloaded{
+          [](const Just<T>& v) { std::cout << "Just " << v << std::endl; },
+          [](Nothing) { std::cout << "Nothing" << std::endl; }},
+      value.value);
+}
+
+int main(int argc, char* argv[]) {
+  auto value = just(3);
+  show(value);
+
+  show((just(3) >>= [](int v) -> Maybe<double> {
+         return {v * 1.5};
+       }) >>= [](double v) -> Maybe<double> { return {v * 1.5}; });
+
+  show((nothing() >>= [](int v) -> Maybe<double> {
+         return {v * 1.5};
+       }) >>= [](double v) -> Maybe<double> { return {v * 1.5}; });
+
+  return 0;
+}
