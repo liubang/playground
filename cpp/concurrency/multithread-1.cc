13c204c6a12df0d6ee8969978f89a232eb045641
liubang <it.liubang@gmail.com>
2020-11-19 18:59:35 +0800

    update

--- cpp/concurrency/multithread-1.cc
+++ cpp/concurrency/multithread-1.cc
@@ -1,15 +1,20 @@
-#include <iostream>
+#include <cstdio>
 #include <string>
 #include <thread>
+#include <vector>
 
-using namespace std;
-
-int main(int argc, char *argv[]) {
-  std::thread t1([] {
-    for (int i = 0; i < 100; i++) {
-      std::cout << "t1:" << i << std::endl;
-    }
-  });
-  t1.join();
+int main(int argc, char* argv[]) {
+  std::vector<std::thread> ts;
+  for (int i = 0; i < 10; ++i) {
+    std::thread t([&] {
+      for (int j = 0; j < 100; j++) {
+        ::printf("[thread %d] - num: %d\n", i, j);
+      }
+    });
+    ts.emplace_back(std::move(t));
+  }
+  for (auto& tt : ts) {
+    tt.join();
+  }
   return 0;
 }
