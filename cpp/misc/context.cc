676b31fd7d0c19355115a726816b4bf7caae2d12
liubang <it.liubang@gmail.com>
2021-01-19 14:02:21 +0800

    update

---
+++ cpp/misc/context.cc
@@ -0,0 +1,52 @@
+#include <ucontext.h>
+#include <cstdio>
+#include <cstdlib>
+
+#define handle_error(msg) \
+  do {                    \
+    perror(msg);          \
+    exit(EXIT_FAILURE);   \
+  } while (00)
+
+namespace {
+ucontext_t uctx_main, uctx_func1, uctx_func2;
+
+void func1() {
+  if (::swapcontext(&uctx_func1, &uctx_func2) == -1)
+    handle_error("swapcontext");
+}
+
+void func2() {
+  if (::swapcontext(&uctx_func2, &uctx_func1) == -1)
+    handle_error("swapcontext");
+}
+} // namespace
+
+int main(int argc, char* argv[]) {
+  char func1_stack[16384];
+  char func2_stack[16384];
+  if (::getcontext(&uctx_func1) == -1)
+    handle_error("getcontext");
+
+  // 设置新的栈
+  uctx_func1.uc_stack.ss_sp = func1_stack;
+  uctx_func1.uc_stack.ss_size = sizeof(func1_stack);
+  // uctx_func1对应的协程执行完执行uctx_main
+  uctx_func1.uc_link = &uctx_main;
+  // 设置协作的工作函数
+  ::makecontext(&uctx_func1, func1, 0);
+  if (::getcontext(&uctx_func2) == -1)
+    handle_error("getcontext");
+  uctx_func2.uc_stack.ss_sp = func2_stack;
+  uctx_func2.uc_stack.ss_size = sizeof(func2_stack);
+  // uctx_func2执行完执行uctx_func1
+  uctx_func2.uc_link = (argc > 1) ? NULL : &uctx_func1;
+  ::makecontext(&uctx_func2, func2, 0);
+
+  // 保存当前执行上下文到uctx_main，然后开始执行uctx_func2对应的上下文
+  if (::swapcontext(&uctx_main, &uctx_func2) == -1)
+    handle_error("swapcontext");
+
+  ::printf("main: exiting\n");
+  return 0;
+}
---
+++ cpp/test/folly/test1.cc
@@ -0,0 +1,38 @@
+#include <folly/Synchronized.h>
+#include <folly/Conv.h>
+#include <unordered_map>
+#include <iostream>
+#include <string>
+
+namespace test {
+folly::Synchronized<std::unordered_map<std::string, std::string>> objs;
+}
+
+int main(int argc, char* argv[]) {
+  test::objs.withWLock([](auto& mmp) {
+    for (int32_t i = 0; i < 100; ++i) {
+      std::string key = folly::to<std::string>("hello", i);
+      mmp[key] = key;
+    }
+  });
+
+  auto mmp = test::objs.withWLock([](auto& mmp) {
+    auto mmmp = mmp;
+    mmp.clear();
+    return mmmp;
+  });
+
+  std::cout << "outter" << '\n';
+  for (auto& m : mmp) {
+    std::cout << m.first << '\n';
+  }
+
+  test::objs.withRLock([](auto& mmp) {
+    std::cout << "inner" << '\n';
+    for (auto& m : mmp) {
+      std::cout << m.first << '\n';
+    }
+  });
+
+  return 0;
+}
