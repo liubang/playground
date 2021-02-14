65d8618c7a902e1b13368bfc593851e14c52eaa6
liubang <it.liubang@gmail.com>
2021-02-14 19:32:03 +0800

    update

---
+++ cpp/highkyck/pool/test.cc
@@ -0,0 +1,22 @@
+#include "object_pool.h"
+#include <iostream>
+
+namespace {
+struct TestObj {
+  std::string name;
+  int32_t age;
+};
+}; // namespace
+
+int main(int argc, char* argv[]) {
+  auto& pool = highkyck::pool::object_pool<TestObj, 10>::get_instance();
+
+  {
+    auto obj = pool.acquire();
+    std::cout << pool.size() << '\n';
+  }
+
+  std::cout << pool.size() << '\n';
+
+  return 0;
+}
