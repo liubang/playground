211f7ba3060b8e01114e3fc79974ead90de1fe71
liubang <it.liubang@gmail.com>
2021-03-09 14:44:11 +0800

    update

---
+++ cpp/leetcode/1047.remove-all-adjacent-duplicates-in-string.cc
@@ -0,0 +1,31 @@
+#include <string>
+#include <gtest/gtest.h>
+
+namespace {
+class Solution {
+ public:
+  std::string removeDuplicates(const std::string& S) {
+    std::string ret;
+    for (auto& ch : S) {
+      if (!ret.empty() && ret.back() == ch) {
+        ret.pop_back();
+      } else {
+        ret.push_back(ch);
+      }
+    }
+    return ret;
+  }
+};
+} // namespace
+
+TEST(Leetcode, remove_all_adjacent_duplicates_in_string) {
+  Solution s;
+  {
+    auto ret = s.removeDuplicates("abbaca");
+    EXPECT_EQ("ca", ret);
+  }
+  {
+    auto ret = s.removeDuplicates("abcddcbabcddcbabc");
+    EXPECT_EQ("abc", ret);
+  }
+}
