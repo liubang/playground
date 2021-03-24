15a4a7e662463f7a1ea3736cce30694fb27d3d15
liubang <it.liubang@gmail.com>
2021-03-24 14:13:50 +0800

    update

---
+++ cpp/leetcode/0302.min-stack-lcci.cc
@@ -0,0 +1,50 @@
+#include <stack>
+#include <climits>
+#include <gtest/gtest.h>
+
+namespace {
+class MinStack {
+ public:
+  MinStack() = default;
+  virtual ~MinStack() = default;
+
+  void push(int x) {
+    st_.emplace(x, std::min(x, getMin()));
+  }
+
+  void pop() {
+    st_.pop();
+  }
+
+  int top() {
+    if (!st_.empty()) {
+      return st_.top().first;
+    }
+    return INT_MIN;
+  }
+
+  int getMin() {
+    if (!st_.empty()) {
+      return st_.top().second;
+    }
+    return INT_MAX;
+  }
+
+ private:
+  std::stack<std::pair<int, int>> st_;
+};
+} // namespace
+
+TEST(Leetcode, min_stack_lcci) {
+  MinStack min_stack;
+  min_stack.push(-2);
+  min_stack.push(0);
+  min_stack.push(-3);
+
+  EXPECT_EQ(-3, min_stack.getMin());
+  EXPECT_EQ(-3, min_stack.top());
+
+  min_stack.pop();
+  EXPECT_EQ(-2, min_stack.getMin());
+  EXPECT_EQ(0, min_stack.top());
+}
