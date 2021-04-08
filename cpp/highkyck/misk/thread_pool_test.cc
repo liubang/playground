dc3c92f3df1dd96fd0585fea7edfda0303fe80bc
liubang <it.liubang@gmail.com>
2021-04-08 15:06:48 +0800

    update

---
+++ cpp/highkyck/misk/thread_pool_test.cc
@@ -0,0 +1,31 @@
+#include <vector>
+#include <future>
+#include <chrono>
+#include <utility>
+#include <gtest/gtest.h>
+
+#include "includes/thread_pool.h"
+
+TEST(thread_pool, thread_pool) {
+  ThreadPool pool(4);
+  std::vector<std::future<std::pair<int, int>>> results;
+
+  auto start = std::chrono::system_clock::now();
+
+  for (int i = 0; i < 1024; ++i) {
+    results.emplace_back(pool.enqueue([i] {
+      std::this_thread::sleep_for(std::chrono::milliseconds(1));
+      return std::make_pair(i, i);
+    }));
+  }
+
+  for (auto& result : results) {
+    std::pair<int, int> pair = result.get();
+    EXPECT_EQ(pair.first, pair.second);
+  }
+
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+      std::chrono::system_clock::now() - start);
+
+  EXPECT_LT(duration.count(), 320);
+}
