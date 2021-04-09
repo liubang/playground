ad6254f6fcb62f53c38ed454fd3bb1ab6de4c3e6
liubang <it.liubang@gmail.com>
2021-04-09 14:27:15 +0800

    update

---
+++ cpp/leetcode/41.first-missing-positive.cc
@@ -0,0 +1,36 @@
+#include <vector>
+#include <algorithm>
+#include <gtest/gtest.h>
+
+namespace {
+class Solution {
+ public:
+  int firstMissingPositive(std::vector<int>& nums) {
+    std::sort(nums.begin(), nums.end());
+    int ret = 1;
+    for (auto num : nums) {
+      if (num <= 0) {
+        continue;
+      }
+      if (num == ret) {
+        ret++;
+      }
+    }
+    return ret;
+  }
+};
+} // namespace
+
+TEST(Leetcode, first_missing_positive) {
+  Solution s;
+
+  {
+    std::vector<int> inputs = {0, 2, 2, 1, 1};
+    EXPECT_EQ(3, s.firstMissingPositive(inputs));
+  }
+
+  {
+    std::vector<int> inputs = {3, 4, -1, 1};
+    EXPECT_EQ(2, s.firstMissingPositive(inputs));
+  }
+}
