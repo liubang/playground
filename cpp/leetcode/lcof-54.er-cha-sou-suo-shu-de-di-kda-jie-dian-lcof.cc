76046b84a89041f920212d64da273bed681cdf71
liubang <it.liubang@gmail.com>
2021-04-09 15:44:20 +0800

    update

---
+++ cpp/leetcode/lcof-54.er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof.cc
@@ -0,0 +1,51 @@
+#include <vector>
+#include <string>
+#include <gtest/gtest.h>
+#include "includes/tree.h"
+
+namespace {
+class Solution {
+ public:
+  using TreeNode = leetcode::tree::TreeNode;
+  int kthLargest(TreeNode* root, int k) {
+    int ret;
+    visit(&ret, root, k);
+    return ret;
+  }
+
+ private:
+  void visit(int* ret, TreeNode* node, int& k) {
+    if (!node || k == 0) {
+      return;
+    }
+    visit(ret, node->right, k);
+    if (k == 0) {
+      return;
+    }
+    if (--k == 0) {
+      *ret = node->val;
+    }
+    visit(ret, node->left, k);
+  }
+};
+} // namespace
+
+TEST(Leetcode, er_cha_sou_suo_shu_de_di_kda_jie_dian_lcof) {
+  Solution s;
+  {
+    std::vector<std::string> nodes = {"3", "1", "4", "null", "2"};
+    auto root = leetcode::tree::create(nodes);
+    EXPECT_EQ(4, s.kthLargest(root, 1));
+    leetcode::tree::destroy(root);
+  }
+
+  {
+    std::vector<std::string> nodes = {
+        "5", "3", "6", "2", "4", "null", "null", "1"};
+
+    auto root = leetcode::tree::create(nodes);
+    leetcode::tree::print(root, leetcode::tree::Order::IN_ORDER);
+    EXPECT_EQ(4, s.kthLargest(root, 3));
+    leetcode::tree::destroy(root);
+  }
+}
