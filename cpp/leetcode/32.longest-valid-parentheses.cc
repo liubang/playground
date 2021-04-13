08201c7754f02e937c708e70f7964a8f2df71452
liubang <it.liubang@gmail.com>
2021-04-13 17:28:25 +0800

    update

---
+++ cpp/leetcode/32.longest-valid-parentheses.cc
@@ -0,0 +1,39 @@
+#include <stack>
+#include <string>
+#include <gtest/gtest.h>
+
+namespace {
+class Solution {
+ public:
+  int longestValidParentheses(const std::string& s) {
+    std::stack<int> stack;
+    stack.push(-1);
+    int ret = 0;
+    for (int i = 0; i < s.length(); ++i) {
+      if (s[i] == '(') {
+        stack.push(i);
+      } else {
+        stack.pop();
+        if (stack.empty()) {
+          stack.push(i);
+        } else {
+          ret = std::max(ret, i - stack.top());
+        }
+      }
+    }
+    return ret;
+  }
+};
+} // namespace
+
+TEST(Leetcode, longest_valid_parentheses) {
+  Solution s;
+  EXPECT_EQ(
+      s.longestValidParentheses(")(()(()(((())(((((()()))((((()()(()()())())"
+                                "())()))()()()())(())()()(((()))))()((()))(("
+                                "(())()((()()())((())))(())))())((()())()()("
+                                "(()((())))))((()(((((()((()))(()()(())))((("
+                                ")))()))())"),
+      132);
+  EXPECT_EQ(4, s.longestValidParentheses(")()())"));
+}
