d977462981490367c733dd4add5866700f33e0ec
liubang <it.liubang@gmail.com>
2021-04-13 18:38:44 +0800

    update

---
+++ cpp/leetcode/58.length-of-last-word.cc
@@ -0,0 +1,27 @@
+#include <string>
+#include <gtest/gtest.h>
+
+namespace {
+class Solution {
+ public:
+  int lengthOfLastWord(const std::string& s) {
+    int i = s.length() - 1;
+    int ret = 0;
+    // 去末尾空格
+    while (i >= 0 && s[i] == ' ') {
+      i--;
+    }
+    while (i >= 0 && s[i] != ' ') {
+      ret++;
+      i--;
+    }
+    return ret;
+  }
+};
+} // namespace
+
+TEST(Leetcode, length_of_last_word) {
+  Solution s;
+  EXPECT_EQ(5, s.lengthOfLastWord("  Hello World  "));
+  EXPECT_EQ(0, s.lengthOfLastWord(" "));
+}
