6202b3a8d6ce26370e29c048e12978092db0c5bd
liubang <it.liubang@gmail.com>
2021-04-14 14:30:46 +0800

    update

---
+++ cpp/leetcode/171.excel-sheet-column-number.cc
@@ -0,0 +1,21 @@
+#include <string>
+#include <gtest/gtest.h>
+
+namespace {
+class Solution {
+ public:
+  int titleToNumber(const std::string& columnTitle) {
+    int ret = 0;
+    for (auto& c : columnTitle) {
+      ret = ret * 26 + (c - 'A' + 1);
+    }
+    return ret;
+  }
+};
+} // namespace
+
+TEST(Leetcode, execl_sheet_column_number) {
+  Solution s;
+  EXPECT_EQ(1, s.titleToNumber("A"));
+  EXPECT_EQ(2321206, s.titleToNumber("EBASD"));
+}
