4bf05928df938808c4e1436d1d643866f4e4bdcd
liubang <it.liubang@gmail.com>
2021-04-14 17:10:32 +0800

    update

--- cpp/leetcode/206.reverse-linked-list.cc
+++ cpp/leetcode/206.reverse-linked-list.cc
@@ -4,6 +4,7 @@
 namespace {
 class Solution {
  public:
+  using ListNode = leetcode::list::ListNode;
   leetcode::list::ListNode* reverseList(leetcode::list::ListNode* head) {
     if (!head) {
       return nullptr;
@@ -18,19 +19,45 @@ class Solution {
     }
     return pre;
   }
+
+  // 递归实现
+  ListNode* reverseList2(ListNode* head) {
+    if (!head || !head->next) {
+      return head;
+    }
+    ListNode* newhead = reverseList(head->next);
+    head->next->next = head;
+    head->next = nullptr;
+    return newhead;
+  }
 };
 } // namespace
 
 TEST(Leetcode, reverse_linked_list) {
   Solution s;
-  leetcode::list::ListNode* head = leetcode::list::create({1, 2, 3, 4, 5});
-  leetcode::list::ListNode* ret = s.reverseList(head);
-  std::vector<int> exp = {5, 4, 3, 2, 1};
-  int i = 0;
-  leetcode::list::ListNode* cur = ret;
-  while (cur) {
-    EXPECT_EQ(exp[i++], cur->val);
-    cur = cur->next;
+  {
+    leetcode::list::ListNode* head = leetcode::list::create({1, 2, 3, 4, 5});
+    leetcode::list::ListNode* ret = s.reverseList(head);
+    std::vector<int> exp = {5, 4, 3, 2, 1};
+    int i = 0;
+    leetcode::list::ListNode* cur = ret;
+    while (cur) {
+      EXPECT_EQ(exp[i++], cur->val);
+      cur = cur->next;
+    }
+    destroy(ret);
   }
-  destroy(ret);
+
+  {
+    leetcode::list::ListNode* head = leetcode::list::create({1, 2, 3, 4, 5});
+    leetcode::list::ListNode* ret = s.reverseList2(head);
+    std::vector<int> exp = {5, 4, 3, 2, 1};
+    int i = 0;
+    leetcode::list::ListNode* cur = ret;
+    while (cur) {
+      EXPECT_EQ(exp[i++], cur->val);
+      cur = cur->next;
+    }
+    destroy(ret);
+  }
 }
