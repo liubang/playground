10a963647c541928e0e038f0f303650a065e53ba
liubang <it.liubang@gmail.com>
2021-06-27 22:46:07 +0800

    update

---
+++ cpp/test/folly/semaphore.cc
@@ -0,0 +1,24 @@
+#include <chrono>
+#include <folly/synchronization/SaturatingSemaphore.h>
+
+namespace test {
+class Foo {
+ public:
+  void run(int millseconds) {
+    semaphore_.try_wait_until(
+        std::chrono::steady_clock::now() +
+        std::chrono::microseconds(millseconds));
+  }
+
+ private:
+  folly::SaturatingSemaphore<true> semaphore_;
+};
+} // namespace test
+
+int main(int argc, char* argv[]) {
+  ::test::Foo foo;
+  for (int i = 0; i < 1000000; ++i) {
+    foo.run(1000);
+  }
+  return 0;
+}
