93d9d1a87168c569a4f2da2df08fe1486382bcf4
liubang <it.liubang@gmail.com>
2021-07-21 16:02:15 +0800

    update

---
+++ cpp/highkyck/misk/test_parameters.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+namespace highkyck {
+class Foo {
+ public:
+  Foo() {
+    std::cout << "构造函数" << '\n';
+  }
+  Foo(const Foo& foo) {
+    std::cout << "复制构造" << '\n';
+  };
+  Foo(const Foo&& foo) {
+    std::cout << "移动构造" << '\n';
+  }
+  Foo& operator=(const Foo& foo) {
+    std::cout << "拷贝构造" << '\n';
+    return *this;
+  }
+  Foo& operator=(const Foo&& foo) {
+    std::cout << "移动拷贝构造" << '\n';
+    return *this;
+  }
+  ~Foo() = default;
+};
+
+class Bar {
+ public:
+  Bar(const Foo& foo) : foo_(foo) {}
+
+ private:
+  Foo foo_;
+};
+} // namespace highkyck
+
+int main(int argc, char* argv[]) {
+  highkyck::Foo foo;
+  { highkyck::Bar bar(foo); }
+  { highkyck::Bar bar(std::move(foo)); }
+  return 0;
+}
