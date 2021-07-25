b404b0b53ebac724b59365b4104a865470550350
liubang <it.liubang@gmail.com>
2021-07-25 15:33:50 +0800

    update

--- cpp/highkyck/misk/test_atomic.cc
+++ cpp/highkyck/misk/test_atomic.cc
@@ -2,21 +2,24 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <vector>
 
 int main(int argc, char* argv[]) {
   std::atomic<int64_t> flag = 0;
-  std::thread t1([&]() {
-    flag++;
-    std::cout << flag << std::endl;
-  });
 
-  std::thread t2([&]() {
-    flag++;
-    std::cout << flag << std::endl;
-  });
+  std::vector<std::thread> threads;
+
+  for (int i = 0; i < 32; ++i) {
+    threads.emplace_back(
+        [&flag]() { flag.fetch_add(1, std::memory_order_relaxed); });
+  }
+
   std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
-  t1.join();
-  t2.join();
+  for (int i = 0; i < 32; ++i) {
+    threads[i].join();
+  }
+
+  std::cout << flag.load(std::memory_order_relaxed) << std::endl;
   return 0;
 }
