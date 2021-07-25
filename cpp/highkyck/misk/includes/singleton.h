5fd69cf05b7b6738a6960c9511282a1ffb3a5f52
liubang <it.liubang@gmail.com>
2021-07-25 15:33:50 +0800

    update

---
+++ cpp/highkyck/misk/includes/singleton.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <atomic>
+#include <mutex>
+
+namespace highkyck{
+namespace misk {
+template <class T>
+class Singleton {
+ public:
+  T* getInstance() {
+    if (ins_ == nullptr) {
+      std::unique_lock<std::mutex> lock(mutex_);
+      if (ins_ == nullptr) {
+        ins_ = new T;
+      }
+    }
+    return ins_;
+  }
+
+ private:
+  std::atomic<T*> ins_{nullptr};
+  std::mutex mutex_;
+};
+} // namespace misk
+} // namespace hhighkyck
