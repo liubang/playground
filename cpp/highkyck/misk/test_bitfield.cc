0bcc947fbd65d30edf9ace19ecf17f7396016b49
liubang <it.liubang@gmail.com>
2021-07-25 14:06:22 +0800

    update

---
+++ cpp/highkyck/misk/test_bitfield.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+namespace highkyck {
+struct S {
+  int c : 9;
+  int d : 7;
+};
+} // namespace highkyck
+
+int main(int argc, char* argv[]) {
+  // 4 Bytes = int
+  // 32 bits
+  std::cout << sizeof(highkyck::S) << "\n";
+  return 0;
+}
