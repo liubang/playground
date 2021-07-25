8bd3d9a86dc1056a74bfa0c8a7d14bc5739a3d05
liubang <it.liubang@gmail.com>
2021-07-25 15:33:50 +0800

    update

---
+++ cpp/highkyck/misk/test_singleton.cc
@@ -0,0 +1,20 @@
+#include "includes/singleton.h"
+#include <iostream>
+
+namespace highkyck {
+class Foo {
+ public:
+  Foo() = default;
+  virtual ~Foo() = default;
+};
+} // namespace highkyck
+
+int main(int argc, char* argv[]) {
+  highkyck::misk::Singleton<highkyck::Foo> foo;
+  highkyck::Foo* f1 = foo.getInstance();
+  highkyck::Foo* f2 = foo.getInstance();
+
+  std::cout << f1 << std::endl;
+  std::cout << f2 << std::endl;
+  return 0;
+}
