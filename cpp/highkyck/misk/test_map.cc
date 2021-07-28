853fd43780ee4bda00f21813e8f958eef434be55
liubang <it.liubang@gmail.com>
2021-07-28 14:29:22 +0800

    update

--- cpp/highkyck/misk/test_map.cc
+++ cpp/highkyck/misk/test_map.cc
@@ -1,8 +1,9 @@
 #include <map>
+#include <unordered_map>
 #include <string>
 #include <iostream>
 
-int main(int argc, char* argv[]) {
+void test1() {
   std::map<std::string, std::string> map;
   try {
     std::cout << map.at("aaa") << '\n';
@@ -13,5 +14,29 @@ int main(int argc, char* argv[]) {
   std::cout << map["aaa"] << '\n';
   std::cout << map.at("aaa") << std::endl;
   std::cout << map.size() << '\n';
+}
+
+class Foo {
+ public:
+  Foo() {
+    aaa_.clear();
+  }
+
+  std::map<std::string, uint64_t> aaa() {
+    return aaa_;
+  }
+
+ private:
+  std::map<std::string, uint64_t> aaa_;
+};
+
+void test2() {
+  Foo foo;
+  auto a = foo.aaa();
+}
+
+int main(int argc, char* argv[]) {
+  test1();
+  test2();
   return 0;
 }
