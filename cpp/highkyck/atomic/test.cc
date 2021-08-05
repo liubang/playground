a5325bfb87ba9d58298ee1bb1fb2043d8fbb8f2d
liubang <it.liubang@gmail.com>
2021-08-05 14:59:48 +0800

    update

---
+++ cpp/highkyck/atomic/test.cc
@@ -0,0 +1,17 @@
+#include <atomic>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  std::atomic<bool> at;
+  at = false;
+
+  bool exp = false;
+  bool ret = at.compare_exchange_strong(exp, true);
+  std::cout << "ret:" << ret << ", at:" << at << std::endl;
+
+  at = false;
+  exp = true;
+  ret = at.compare_exchange_strong(exp, false);
+  std::cout << "ret:" << ret << ", at:" << at << std::endl;
+  return 0;
+}
