ba847a603e65c63bbdc070e84e134c8b9cf31c55
liubang <it.liubang@gmail.com>
2021-11-16 16:45:31 +0800

    update

---
+++ go/mix/demo9/main.go
@@ -0,0 +1,40 @@
+// Copyright (c) 2021 The Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Authors: liubang (it.liubang@gmail.com)
+// Created: 2021/11/16 16:45
+
+package main
+
+import (
+	"log"
+	"math"
+)
+
+func main() {
+	// nss := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+	nss := []string{}
+	groups := math.Ceil(float64(len(nss)) / 3.00)
+	log.Println("len:", len(nss))
+	log.Println("groups: ", groups)
+	for i := 0; i < int(groups); i++ {
+		var ns []string
+		if i != int(groups)-1 {
+			ns = nss[i*3 : (i+1)*3]
+		} else {
+			ns = nss[i*3:]
+		}
+		log.Println(ns)
+	}
+}
