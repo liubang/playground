f6db53caf4c2e67961d2f6606e1f090043b25ea9
liubang <it.liubang@gmail.com>
2021-11-28 22:44:30 +0800

    update

---
+++ cpp/basecode/logger/log_appender.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <condition_variable>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <unistd.h>
+#include <vector>
+
+#include "buffer.h"
+#include "count_down_latch.h"
+
+namespace basecode {
+class LogAppenderInterface
+{
+public:
+    virtual ~LogAppenderInterface() {}
+
+    virtual void append(const char* msg, size_t len) = 0;
+
+public:
+    using Ptr = std::shared_ptr<LogAppenderInterface>;
+};
+
+
+class AsyncLogAppender : public LogAppenderInterface
+{
+public:
+    AsyncLogAppender(const std::string& basename);
+
+    ~AsyncLogAppender();
+
+    void append(const char* msg, size_t len);
+
+    void start();
+
+    void stop();
+
+private:
+    void thread_func();
+
+private:
+    bool                                 started_;
+    bool                                 running_;
+    time_t                               persist_period_;
+    std::string                          basename_;
+    std::mutex                           mutex_;
+    std::condition_variable              cound_;
+    CountDownLatch                       countdown_latch_;
+    std::thread                          persist_thread_;
+    std::unique_ptr<Buffer>              cur_buffer_;
+    std::vector<std::unique_ptr<Buffer>> buffers_;
+};
+}  // namespace basecode
