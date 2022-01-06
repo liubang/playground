bfe789aa330f47e70e8c3651eac5b9c2ae1f5673
liubang <it.liubang@gmail.com>
2022-01-06 12:19:03 +0800

    update

--- cpp/highkyck/arena/arena_test.cpp
+++ cpp/highkyck/arena/arena_test.cpp
@@ -1,1 +1,19 @@
+#include "arena.h"
+
 #include <gtest/gtest.h>
+
+TEST(arena, allocate) {
+  constexpr std::size_t ptr_char_size = sizeof(char*);
+
+  highkyck::Arena arena;
+  arena.allocate(1024);
+  int usage = 4096 + ptr_char_size;
+  EXPECT_EQ(arena.memory_usage(), usage);
+
+  arena.allocate(2048);
+  EXPECT_EQ(arena.memory_usage(), usage);
+
+  arena.allocate(2048);
+  usage += 2048 + ptr_char_size;
+  EXPECT_EQ(arena.memory_usage(), usage);
+}
