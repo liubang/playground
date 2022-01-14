c218ca3ef5994bbf4c5d6f7ed7e91e6cee7b2e3e
liubang <it.liubang@gmail.com>
2022-01-14 10:40:49 +0800

    backup

---
+++ cpp/highkyck/sstable/iterator.cpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "iterator.h"
+
+namespace highkyck {
+namespace sstable {
+
+Iterator::Iterator() {}
+
+Iterator::~Iterator() {
+  while (!cleanup_list_.empty()) {
+    CleanupNode *node = cleanup_list_.front();
+    cleanup_list_.pop_front();
+    node->Run();
+    delete node;
+  }
+}
+
+void Iterator::RegisterCleanup(CleanupFunction function, void *arg1,
+                               void *arg2) {
+  assert(function != nullptr);
+  CleanupNode *node = new CleanupNode;
+  node->function = function;
+  node->arg1 = arg1;
+  node->arg2 = arg2;
+  cleanup_list_.push_back(node);
+}
+
+}  // namespace sstable
+}  // namespace highkyck
