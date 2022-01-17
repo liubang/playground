119984de06d9297d4d7024ea3a7742a2729b9453
liubang <it.liubang@gmail.com>
2022-01-18 00:35:50 +0800

    backup

--- cpp/highkyck/bfcc/bfcc.cpp
+++ cpp/highkyck/bfcc/bfcc.cpp
@@ -12,14 +12,5 @@
 
 int main(int argc, char* argv[]) {
   // put your code here
-  const char* code = " 125 + 1 - 3 * 4 / 2";
-
-  highkyck::bfcc::Lexer lexer(code);
-
-  do {
-    lexer.GetNextToken();
-    std::cout << lexer.CurrentToken()->Content() << std::endl;
-  } while (lexer.CurrentToken()->Kind() != highkyck::bfcc::TokenKind::Eof);
-
   return 0;
 }
