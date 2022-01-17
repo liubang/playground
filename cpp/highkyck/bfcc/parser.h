cb1d9aa14efc4ea6b32ff0f590620ce6042b2ea6
liubang <it.liubang@gmail.com>
2022-01-18 00:35:50 +0800

    backup

---
+++ cpp/highkyck/bfcc/parser.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory>
+
+#include "ast_node.h"
+#include "lexer.h"
+
+namespace highkyck {
+namespace bfcc {
+
+class Parser {
+ public:
+  Parser(std::shared_ptr<Lexer> lexer_ptr) : lexer_ptr_(lexer_ptr) {}
+  std::shared_ptr<AstNode> Parse();
+
+ private:
+  std::shared_ptr<AstNode> ParseExpr();
+  std::shared_ptr<AstNode> ParseAddExpr();
+  std::shared_ptr<AstNode> ParseMultiExpr();
+  std::shared_ptr<AstNode> ParsePrimaryExpr();
+
+ private:
+  std::shared_ptr<Lexer> lexer_ptr_;
+};
+
+}  // namespace bfcc
+}  // namespace highkyck
