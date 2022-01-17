b004f4232091214691f9a06e1eeee84e345721fa
liubang <it.liubang@gmail.com>
2022-01-17 14:04:18 +0800

    update

--- cpp/highkyck/bfcc/ut/lexer_test.cpp
+++ cpp/highkyck/bfcc/ut/lexer_test.cpp
@@ -2,4 +2,67 @@
 
 #include <gtest/gtest.h>
 
-TEST(Lexer, GetNextToken) {}
+TEST(Lexer, GetNextToken) {
+  const char* code = " 125 + 1 - 3 * 4 / 2";
+  highkyck::bfcc::Lexer lexer(code);
+
+  // 125
+  lexer.GetNextToken();
+  EXPECT_EQ(lexer.CurrentToken()->Kind(), highkyck::bfcc::TokenKind::Num);
+  EXPECT_EQ(lexer.CurrentToken()->Value(), 125);
+  EXPECT_EQ(lexer.CurrentToken()->Content(), "125");
+
+  // +
+  lexer.GetNextToken();
+  EXPECT_EQ(lexer.CurrentToken()->Kind(), highkyck::bfcc::TokenKind::Add);
+  EXPECT_EQ(lexer.CurrentToken()->Value(), 0);
+  EXPECT_EQ(lexer.CurrentToken()->Content(), "+");
+
+  // 1
+  lexer.GetNextToken();
+  EXPECT_EQ(lexer.CurrentToken()->Kind(), highkyck::bfcc::TokenKind::Num);
+  EXPECT_EQ(lexer.CurrentToken()->Value(), 1);
+  EXPECT_EQ(lexer.CurrentToken()->Content(), "1");
+
+  // -
+  lexer.GetNextToken();
+  EXPECT_EQ(lexer.CurrentToken()->Kind(), highkyck::bfcc::TokenKind::Sub);
+  EXPECT_EQ(lexer.CurrentToken()->Value(), 0);
+  EXPECT_EQ(lexer.CurrentToken()->Content(), "-");
+
+  // 3
+  lexer.GetNextToken();
+  EXPECT_EQ(lexer.CurrentToken()->Kind(), highkyck::bfcc::TokenKind::Num);
+  EXPECT_EQ(lexer.CurrentToken()->Value(), 3);
+  EXPECT_EQ(lexer.CurrentToken()->Content(), "3");
+
+  // *
+  lexer.GetNextToken();
+  EXPECT_EQ(lexer.CurrentToken()->Kind(), highkyck::bfcc::TokenKind::Mul);
+  EXPECT_EQ(lexer.CurrentToken()->Value(), 0);
+  EXPECT_EQ(lexer.CurrentToken()->Content(), "*");
+
+  // 4
+  lexer.GetNextToken();
+  EXPECT_EQ(lexer.CurrentToken()->Kind(), highkyck::bfcc::TokenKind::Num);
+  EXPECT_EQ(lexer.CurrentToken()->Value(), 4);
+  EXPECT_EQ(lexer.CurrentToken()->Content(), "4");
+
+  // /
+  lexer.GetNextToken();
+  EXPECT_EQ(lexer.CurrentToken()->Kind(), highkyck::bfcc::TokenKind::Div);
+  EXPECT_EQ(lexer.CurrentToken()->Value(), 0);
+  EXPECT_EQ(lexer.CurrentToken()->Content(), "/");
+
+  // 2
+  lexer.GetNextToken();
+  EXPECT_EQ(lexer.CurrentToken()->Kind(), highkyck::bfcc::TokenKind::Num);
+  EXPECT_EQ(lexer.CurrentToken()->Value(), 2);
+  EXPECT_EQ(lexer.CurrentToken()->Content(), "2");
+
+  // Eof
+  lexer.GetNextToken();
+  EXPECT_EQ(lexer.CurrentToken()->Kind(), highkyck::bfcc::TokenKind::Eof);
+  EXPECT_EQ(lexer.CurrentToken()->Value(), 0);
+  EXPECT_EQ(lexer.CurrentToken()->Content(), "");
+}
