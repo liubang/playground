aae668f14142e8b5b35f4ece8c08a2fe6877567e
liubang <it.liubang@gmail.com>
2022-01-21 17:01:16 +0800

    backup

--- cpp/highkyck/common/cli_table_test.cpp
+++ cpp/highkyck/common/cli_table_test.cpp
@@ -3,13 +3,18 @@
 int main(int argc, char* argv[]) {
   highkyck::common::CliTable table;
   table.Reset(3);
+  table.Cell("aaaaaa").Cell("bbbbb").Cell("cccc").Next();
+  table.Cell("aaaaaa").Cell("bbbbb").Cell("cccc").Next();
+  table.Cell("aaaaaa").Cell("bbsaqdbbb").Cell("ccccdfasdfaf").Next();
+  table.Cell("aaaaaa").Cell("bbbbllllllllllllllllllb").Cell("cccc").Next();
+  table.Cell("adsfakljklwqwerwqaaaaa").Cell("bbbbb").Cell("cccdjalfdwqac");
+  table.Print();
 
-  table.Add("aaaaaa").Add("bbbbb").Add("cccc").Next();
-  table.Add("aaaaaa").Add("bbbbb").Add("cccc").Next();
-  table.Add("aaaaaa").Add("bbsaqdbbb").Add("ccccdfasdfaf").Next();
-  table.Add("aaaaaa").Add("bbbbllllllllllllllllllb").Add("cccc").Next();
-  table.Add("adsfakljklwqwerwqaaaaa").Add("bbbbb").Add("cccdjalfdwqac");
-
+  table.Reset(4);
+  table.Cell("id").Cell("name").Cell("age").Cell("description").Next();
+  table.Cell("0000001").Cell("zhangsan").Cell("20").Cell("this is zhangsan").Next();
+  table.Cell("0000002").Cell("lisi").Cell("30").Cell("this is lisi").Next();
+  table.Cell("0000003").Cell("wangwu").Cell("25").Cell("this is wangwu").Next();
   table.Print();
 
   return 0;
