c41fe4c19a00ceac028da12c92b3decd5e09f12d
liubang <it.liubang@gmail.com>
2022-01-21 10:29:11 +0800

    update

--- cpp/highkyck/common/cli_table.h
+++ cpp/highkyck/common/cli_table.h
@@ -17,65 +17,66 @@ class CliTable {
     cell_size_ = size;
     cell_maxlens_.clear();
     cell_maxlens_.resize(size, 0);
-    lines_.clear();
-    lines_.resize(1);
+    rows_.clear();
+    rows_.resize(1);
   }
 
   CliTable& Add(const std::string& cell) {
-    Line& line = lines_.back();
-    assert(line.size() <= cell_size_);
-    line.push_back(cell);
-    if (cell.length() > cell_maxlens_[line.size() - 1]) {
-      cell_maxlens_[line.size() - 1] = cell.size();
+    Row& row = rows_.back();
+    assert(row.size() <= cell_size_);
+    row.push_back(cell);
+    if ((cell.size() + 2) > cell_maxlens_[row.size() - 1]) {
+      cell_maxlens_[row.size() - 1] = cell.size() + 2;
     }
     return *this;
   }
 
   // add new line
   CliTable& Next() {
-    lines_.push_back({});
+    rows_.push_back({});
     return *this;
   }
 
   void Print() {
-    for (std::size_t i = 0; i < lines_.size(); ++i) {
-      for (std::size_t j = 0; j < cell_maxlens_.size(); ++j) {
-        if (j == 0) ::fprintf(stdout, "+");
-        for (std::size_t n = 0; n < cell_maxlens_[j]; ++n) {
-          ::fprintf(stdout, "-");
-        }
-        ::fprintf(stdout, "+");
-      }
-      ::fprintf(stdout, "\n");
-      for (std::size_t j = 0; j < lines_[i].size(); ++j) {
-        ::fprintf(stdout, "|");
-        ::fprintf(stdout, lines_[i][j].data());
-        if (lines_[i][j].size() < cell_maxlens_[j]) {
-          for (auto n = 0; n < cell_maxlens_[j] - lines_[i][j].size(); ++n) {
-            ::fprintf(stdout, " ");
-          }
-        }
-      }
-      ::fprintf(stdout, "|\n");
+    for (const auto& row : rows_) {
+      PrintLine();
+      PrintRow(row);
     }
-    for (std::size_t j = 0; j < cell_maxlens_.size(); ++j) {
-      if (j == 0) ::fprintf(stdout, "+");
-      for (std::size_t n = 0; n < cell_maxlens_[j]; ++n) {
+    PrintLine();
+    Reset(0);
+  }
+
+ private:
+  using Row = std::vector<std::string>;
+
+  void PrintRow(const Row& row) {
+    for (std::size_t i = 0; i < row.size(); ++i) {
+      ::fprintf(stdout, "| ");
+      ::fprintf(stdout, "%s ", row[i].data());
+      if ((row[i].size() + 2) < cell_maxlens_[i]) {
+        for (auto n = 0; n < cell_maxlens_[i] - row[i].size() - 2; ++n) {
+          ::fprintf(stdout, " ");
+        }
+      }
+    }
+    ::fprintf(stdout, "|\n");
+  }
+
+  void PrintLine() {
+    for (std::size_t i = 0; i < cell_maxlens_.size(); ++i) {
+      if (i == 0) ::fprintf(stdout, "+");
+      for (std::size_t n = 0; n < cell_maxlens_[i]; ++n) {
         ::fprintf(stdout, "-");
       }
       ::fprintf(stdout, "+");
     }
     ::fprintf(stdout, "\n");
-    Reset(0);
   }
 
  private:
-  using Line = std::vector<std::string>;
-
- private:
-  std::size_t cell_size_;
+  std::size_t cell_size_;  // the number of cells per row
   std::vector<std::size_t> cell_maxlens_;
-  std::vector<Line> lines_;
+  std::vector<Row> rows_;
 };
 
 }  // namespace common
