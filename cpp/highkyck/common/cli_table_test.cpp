b8b28e34ae6a9a12f33e33b11d3e4ec90c3e2b02
liubang <it.liubang@gmail.com>
2022-01-24 10:14:01 +0800

    update

--- cpp/highkyck/common/cli_table_test.cpp
+++ cpp/highkyck/common/cli_table_test.cpp
@@ -3,19 +3,37 @@
 int main(int argc, char* argv[]) {
   highkyck::common::CliTable table;
   table.Reset(3);
-  table.Cell("aaaaaa").Cell("bbbbb").Cell("cccc").Next();
-  table.Cell("aaaaaa").Cell("bbbbb").Cell("cccc").Next();
-  table.Cell("aaaaaa").Cell("bbsaqdbbb").Cell("ccccdfasdfaf").Next();
-  table.Cell("aaaaaa").Cell("bbbbllllllllllllllllllb").Cell("cccc").Next();
+  table.Cell("aaaaaa").Cell("bbbbb").Cell("cccc").NewRow();
+  table.Cell("aaaaaa").Cell("bbbbb").Cell("cccc").NewRow();
+  table.Cell("aaaaaa").Cell("bbsaqdbbb").Cell("ccccdfasdfaf").NewRow();
+  table.Cell("aaaaaa").Cell("bbbbllllllllllllllllllb").Cell("cccc").NewRow();
   table.Cell("adsfakljklwqwerwqaaaaa").Cell("bbbbb").Cell("cccdjalfdwqac");
   table.Print();
 
   table.Reset(4);
-  table.Cell("id").Cell("name").Cell("age").Cell("description").Next();
-  table.Cell("0000001").Cell("zhangsan").Cell("20").Cell("this is zhangsan").Next();
-  table.Cell("0000002").Cell("lisi").Cell("30").Cell("this is lisi").Next();
-  table.Cell("0000003").Cell("wangwu").Cell("25").Cell("this is wangwu").Next();
-  table.Print();
+  table.Cell("id")
+      .Cell("name")
+      .Cell("age")
+      .Cell("description")
+      .NewRow()
+      .Cell("0000001")
+      .Cell("zhangsan")
+      .Cell("20")
+      .Cell("this is zhangsan")
+      .NewRow()
+      .Cell("0000002")
+      .Cell("lisi")
+      .Cell("30")
+      .Cell("this is lisi")
+      .NewRow()
+      .Cell("0000003")
+      .Cell("wangwu")
+      .Cell("25")
+      .Cell("this is wangwu")
+      .NewRow()
+      .Cell("0000004")
+      .Cell("张三").Cell("22").Cell("这是张三的档案啊")
+      .Print();
 
   return 0;
 }
