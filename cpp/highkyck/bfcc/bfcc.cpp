f6a16ed03752fbe90198e1fb1c10f2e90b206849
liubang <it.liubang@gmail.com>
2022-01-25 00:35:30 +0800

    update

--- cpp/highkyck/bfcc/bfcc.cpp
+++ cpp/highkyck/bfcc/bfcc.cpp
@@ -8,6 +8,7 @@
 //=====================================================================
 #include <iostream>
 
+#include "codegen.h"
 #include "lexer.h"
 #include "parser.h"
 #include "print_visitor.h"
@@ -17,15 +18,28 @@ static constexpr char code[] = "  5 + 1- 3*4/2 ";
 using Lexer = highkyck::bfcc::Lexer;
 using Parser = highkyck::bfcc::Parser;
 using PrintVisitor = highkyck::bfcc::PrintVisitor;
+using CodeGen = highkyck::bfcc::CodeGen;
 
-int main(int argc, char* argv[]) {
-  // put your code here
+void test_lexer() {
   Lexer lexer(code);
   lexer.GetNextToken();
   Parser parser(&lexer);
   PrintVisitor visitor;
   auto root = parser.Parse();
   root->Accept(&visitor);
+}
 
+void test_codegen() {
+  Lexer lexer(code);
+  lexer.GetNextToken();
+  Parser parser(&lexer);
+  CodeGen codegen;
+  auto root = parser.Parse();
+  root->Accept(&codegen);
+}
+
+int main(int argc, char* argv[]) {
+  // put your code here
+  test_codegen();
   return 0;
 }
