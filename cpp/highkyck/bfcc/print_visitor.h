e5bfd155a8ee05e103cd7659d04c8496a5fa07bc
liubang <it.liubang@gmail.com>
2022-01-27 11:31:55 +0800

    update

--- cpp/highkyck/bfcc/print_visitor.h
+++ cpp/highkyck/bfcc/print_visitor.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <sstream>
+#include <string>
+#include <vector>
+
 #include "ast_node.h"
 
 namespace highkyck {
@@ -8,10 +12,19 @@ namespace bfcc {
 class PrintVisitor : public AstVisitor {
  public:
   void VisitorProgram(ProgramNode* node) override;
+  void Descripbe() const;
+  std::string String() const;
+  virtual ~PrintVisitor() {
+    sstream_.str("");
+    sstream_.clear();
+  }
 
  private:
   void VisitorBinaryNode(BinaryNode* node) override;
   void VisitorConstantNode(ConstantNode* node) override;
+
+ private:
+  std::stringstream sstream_;
 };
 
 }  // namespace bfcc
