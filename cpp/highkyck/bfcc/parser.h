36a9dc78897b7806495d760a0f35c9016c8f7165
liubang <it.liubang@gmail.com>
2022-02-15 17:00:53 +0800

    update

--- cpp/highkyck/bfcc/parser.h
+++ cpp/highkyck/bfcc/parser.h
@@ -9,28 +9,31 @@
 namespace highkyck {
 namespace bfcc {
 
-class Parser {
- public:
-  explicit Parser(Lexer* lexer_ptr) : lexer_ptr_(lexer_ptr) {}
-  std::shared_ptr<ProgramNode> Parse();
+class Parser
+{
+public:
+    explicit Parser(Lexer* lexer_ptr)
+        : lexer_ptr_(lexer_ptr)
+    {}
+    std::shared_ptr<ProgramNode> Parse();
 
- private:
-  std::shared_ptr<AstNode> ParseStmt();
-  std::shared_ptr<AstNode> ParseExpr();
-  std::shared_ptr<AstNode> ParseAssignExpr();
-  std::shared_ptr<AstNode> ParseEqualExpr();
-  std::shared_ptr<AstNode> ParseRelationalExpr();
-  std::shared_ptr<AstNode> ParseAddExpr();
-  std::shared_ptr<AstNode> ParseMultiExpr();
-  std::shared_ptr<AstNode> ParsePrimaryExpr();
-  std::shared_ptr<Identifier> FindId(std::string_view name);
-  std::shared_ptr<Identifier> MakeId(std::string_view name);
+private:
+    std::shared_ptr<AstNode>    ParseStmt();
+    std::shared_ptr<AstNode>    ParseExpr();
+    std::shared_ptr<AstNode>    ParseAssignExpr();
+    std::shared_ptr<AstNode>    ParseEqualExpr();
+    std::shared_ptr<AstNode>    ParseRelationalExpr();
+    std::shared_ptr<AstNode>    ParseAddExpr();
+    std::shared_ptr<AstNode>    ParseMultiExpr();
+    std::shared_ptr<AstNode>    ParsePrimaryExpr();
+    std::shared_ptr<Identifier> FindId(std::string_view name);
+    std::shared_ptr<Identifier> MakeId(std::string_view name);
 
- private:
-  Lexer* lexer_ptr_;
-  std::list<std::shared_ptr<Identifier>>* ids_{nullptr};
-  std::unordered_map<std::string_view, std::shared_ptr<Identifier>>
-      ids_map_;  // for search
+private:
+    Lexer*                                  lexer_ptr_;
+    std::list<std::shared_ptr<Identifier>>* ids_{nullptr};
+    std::unordered_map<std::string_view, std::shared_ptr<Identifier>>
+        ids_map_;  // for search
 };
 
 }  // namespace bfcc
--- cpp/highkyck/bfcc/print_visitor.h
+++ cpp/highkyck/bfcc/print_visitor.h
@@ -9,25 +9,30 @@
 namespace highkyck {
 namespace bfcc {
 
-class PrintVisitor : public AstVisitor {
- public:
-  void VisitorProgram(ProgramNode* node) override;
-  void Descripbe() const;
-  std::string String() const;
-  virtual ~PrintVisitor() {
-    sstream_.str("");
-    sstream_.clear();
-  }
+class PrintVisitor : public AstVisitor
+{
+public:
+    void VisitorProgram(ProgramNode* node) override;
 
- private:
-  void VisitorExprStmtNode(ExprStmtNode* node) override;
-  void VisitorAssignStmtNode(AssignExprNode* node) override;
-  void VisitorBinaryNode(BinaryNode* node) override;
-  void VisitorIdentifierNode(IdentifierNode* node) override;
-  void VisitorConstantNode(ConstantNode* node) override;
+    void Descripbe() const;
 
- private:
-  std::stringstream sstream_;
+    std::string String() const;
+
+    virtual ~PrintVisitor()
+    {
+        sstream_.str("");
+        sstream_.clear();
+    }
+
+private:
+    void VisitorExprStmtNode(ExprStmtNode* node) override;
+    void VisitorAssignStmtNode(AssignExprNode* node) override;
+    void VisitorBinaryNode(BinaryNode* node) override;
+    void VisitorIdentifierNode(IdentifierNode* node) override;
+    void VisitorConstantNode(ConstantNode* node) override;
+
+private:
+    std::stringstream sstream_;
 };
 
 }  // namespace bfcc
