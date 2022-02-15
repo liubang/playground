bdf5b1f212b891f961e25c7a0b04eac8ebd2fe08
liubang <it.liubang@gmail.com>
2022-02-15 17:00:53 +0800

    update

--- cpp/highkyck/bfcc/bfcc.cpp
+++ cpp/highkyck/bfcc/bfcc.cpp
@@ -10,14 +10,15 @@
 
 #include "lexer.h"
 
-int main(int argc, char* argv[]) {
-  // put your code here
-  const char* source = argv[1];
-  highkyck::bfcc::Lexer lexer(source);
-  do {
-    lexer.GetNextToken();
-    std::cout << *(lexer.CurrentToken()) << std::endl;
-  } while (lexer.CurrentToken()->Type() != highkyck::bfcc::TokenType::Eof);
+int main(int argc, char* argv[])
+{
+    // put your code here
+    const char*           source = argv[1];
+    highkyck::bfcc::Lexer lexer(source);
+    do {
+        lexer.GetNextToken();
+        std::cout << *(lexer.CurrentToken()) << std::endl;
+    } while (lexer.CurrentToken()->Type() != highkyck::bfcc::TokenType::Eof);
 
-  return 0;
+    return 0;
 }
