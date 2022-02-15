a970e5357f9b5e8de4fe78ede32e1cf76ecef230
liubang <it.liubang@gmail.com>
2022-02-15 18:01:18 +0800

    update

--- cpp/highkyck/bfcc/lexer.cpp
+++ cpp/highkyck/bfcc/lexer.cpp
@@ -10,6 +10,7 @@
 
 namespace {
 
+/* clang-format off */
 constexpr char BFCC_CHAR_EOF       = '\0';
 constexpr char BFCC_CHAR_ADD       = '+';
 constexpr char BFCC_CHAR_SUB       = '-';
@@ -22,177 +23,158 @@ constexpr char BFCC_CHAR_ASSIGN    = '=';
 constexpr char BFCC_CHAR_NOT       = '!';
 constexpr char BFCC_CHAR_GREATER   = '>';
 constexpr char BFCC_CHAR_LESSER    = '<';
+/* clang-format on */
 
 }  // namespace
 
 namespace highkyck {
 namespace bfcc {
 
-void Lexer::GetNextChar()
-{
-    if (cursor_ == source_code_.size()) {
-        cur_char_ = '\0';
-        cursor_++;
-    } else {
-        assert(cursor_ < source_code_.size());
-        cur_char_ = source_code_[cursor_++];
+void Lexer::GetNextChar() {
+  if (cursor_ == source_code_.size()) {
+    cur_char_ = '\0';
+    cursor_++;
+  } else {
+    assert(cursor_ < source_code_.size());
+    cur_char_ = source_code_[cursor_++];
+  }
+}
+
+void Lexer::ExpectToken(TokenType type) {
+  if (CurrentToken()->type == type) {
+    GetNextToken();
+  } else {
+    DiagnosticError(source_code_, line_, CurrentToken()->location.col,
+                    "'%s' expected", TokenTypeName(type).data());
+  }
+}
+
+char Lexer::PeekChar(int distance) {
+  assert(distance >= 0);
+  std::size_t idx = cursor_ - 1 + distance;
+  if (idx < source_code_.size()) {
+    return source_code_[idx];
+  } else {
+    return '\0';
+  }
+}
+
+bool Lexer::IsLetter() {
+  return std::isalpha(cur_char_) || cur_char_ == '_';
+}
+
+bool Lexer::IsDigit() {
+  return std::isdigit(cur_char_);
+}
+
+bool Lexer::IsLetterOrDigit() {
+  return IsLetter() || IsDigit();
+}
+
+void Lexer::GetNextToken() {
+  // sksip white space
+  while (::isspace(cur_char_)) {
+    if (cur_char_ == '\n') {
+      line_++;
+      line_head_ = cursor_;
     }
-}
+    GetNextChar();
+  }
 
-void Lexer::ExpectToken(TokenType type)
-{
-    if (CurrentToken()->Type() == type) {
-        GetNextToken();
-    } else {
-        DiagnosticError(source_code_,
-                        line_,
-                        CurrentToken()->Location().col,
-                        "'%s' expected",
-                        TokenTypeName(type).data());
-    }
-}
+  TokenType kind;
+  SourceLocation location = {.line = line_, .col = cursor_ - 1 - line_head_};
+  int64_t value = 0;
+  int64_t start_pos = cursor_ - 1;
 
-char Lexer::PeekChar(int distance)
-{
-    assert(distance >= 0);
-    std::size_t idx = cursor_ - 1 + distance;
-    if (idx < source_code_.size()) {
-        return source_code_[idx];
-    } else {
-        return '\0';
-    }
-}
-
-bool Lexer::IsLetter()
-{
-    return std::isalpha(cur_char_) || cur_char_ == '_';
-}
-
-bool Lexer::IsDigit()
-{
-    return std::isdigit(cur_char_);
-}
-
-bool Lexer::IsLetterOrDigit()
-{
-    return IsLetter() || IsDigit();
-}
-
-void Lexer::GetNextToken()
-{
-    // sksip white space
-    while (::isspace(cur_char_)) {
-        if (cur_char_ == '\n') {
-            line_++;
-            line_head_ = cursor_;
-        }
-        GetNextChar();
-    }
-
-    TokenType      kind;
-    SourceLocation location;
-    location.line     = line_;                     // 当前行
-    location.col      = cursor_ - 1 - line_head_;  // 当前列
-    int64_t value     = 0;
-    int64_t start_pos = cursor_ - 1;
-
-    switch (cur_char_) {
+  switch (cur_char_) {
     case BFCC_CHAR_EOF: kind = TokenType::Eof; break;
     case BFCC_CHAR_ADD:
-        kind = TokenType::Add;
-        GetNextChar();
-        break;
+      kind = TokenType::Add;
+      GetNextChar();
+      break;
     case BFCC_CHAR_SUB:
-        kind = TokenType::Sub;
-        GetNextChar();
-        break;
+      kind = TokenType::Sub;
+      GetNextChar();
+      break;
     case BFCC_CHAR_MUL:
-        kind = TokenType::Mul;
-        GetNextChar();
-        break;
+      kind = TokenType::Mul;
+      GetNextChar();
+      break;
     case BFCC_CHAR_DIV:
-        kind = TokenType::Div;
-        GetNextChar();
-        break;
+      kind = TokenType::Div;
+      GetNextChar();
+      break;
     case BFCC_CHAR_LPARENT:
-        kind = TokenType::LParent;
-        GetNextChar();
-        break;
+      kind = TokenType::LParent;
+      GetNextChar();
+      break;
     case BFCC_CHAR_RPARENT:
-        kind = TokenType::RParent;
-        GetNextChar();
-        break;
+      kind = TokenType::RParent;
+      GetNextChar();
+      break;
     case BFCC_CHAR_SEMICOLON:
-        kind = TokenType::Semicolon;
-        GetNextChar();
-        break;
+      kind = TokenType::Semicolon;
+      GetNextChar();
+      break;
     case BFCC_CHAR_ASSIGN:
-        if (PeekChar(1) == BFCC_CHAR_ASSIGN) {
-            GetNextChar();
-            kind = TokenType::Equal;
-        } else {
-            kind = TokenType::Assign;
-        }
+      if (PeekChar(1) == BFCC_CHAR_ASSIGN) {
         GetNextChar();
-        break;
+        kind = TokenType::Equal;
+      } else {
+        kind = TokenType::Assign;
+      }
+      GetNextChar();
+      break;
     case BFCC_CHAR_NOT:
-        if (PeekChar(1) == BFCC_CHAR_ASSIGN) {
-            GetNextChar();
-            kind = TokenType::PipeEqual;
-        } else {
-            DiagnosticError(source_code_,
-                            location.line,
-                            location.col,
-                            "current '%c' is illegal",
-                            cur_char_);
-        }
+      if (PeekChar(1) == BFCC_CHAR_ASSIGN) {
         GetNextChar();
-        break;
+        kind = TokenType::PipeEqual;
+      } else {
+        DiagnosticError(source_code_, location.line, location.col,
+                        "current '%c' is illegal", cur_char_);
+      }
+      GetNextChar();
+      break;
     case BFCC_CHAR_GREATER:
-        if (PeekChar(1) == BFCC_CHAR_ASSIGN) {
-            GetNextChar();
-            kind = TokenType::GreaterEqual;
-        } else {
-            kind = TokenType::Greater;
-        }
+      if (PeekChar(1) == BFCC_CHAR_ASSIGN) {
         GetNextChar();
-        break;
+        kind = TokenType::GreaterEqual;
+      } else {
+        kind = TokenType::Greater;
+      }
+      GetNextChar();
+      break;
     case BFCC_CHAR_LESSER:
-        if (PeekChar(1) == BFCC_CHAR_ASSIGN) {
-            GetNextChar();
-            kind = TokenType::LesserEqual;
-        } else {
-            kind = TokenType::Lesser;
-        }
+      if (PeekChar(1) == BFCC_CHAR_ASSIGN) {
         GetNextChar();
-        break;
+        kind = TokenType::LesserEqual;
+      } else {
+        kind = TokenType::Lesser;
+      }
+      GetNextChar();
+      break;
     default:
-        if (IsLetter()) {
-            while (IsLetterOrDigit()) {
-                GetNextChar();
-            }
-            kind = TokenType::Identifier;
-        } else if (IsDigit()) {
-            do {
-                value = value * 10 + cur_char_ - '0';
-                GetNextChar();
-            } while (IsDigit());
-            kind = TokenType::Num;
-        } else {
-            DiagnosticError(source_code_,
-                            location.line,
-                            location.col,
-                            "current '%c' is illegal",
-                            cur_char_);
+      if (IsLetter()) {
+        while (IsLetterOrDigit()) {
+          GetNextChar();
         }
-        break;
-    }
+        kind = TokenType::Identifier;
+      } else if (IsDigit()) {
+        do {
+          value = value * 10 + cur_char_ - '0';
+          GetNextChar();
+        } while (IsDigit());
+        kind = TokenType::Num;
+      } else {
+        DiagnosticError(source_code_, location.line, location.col,
+                        "current '%c' is illegal", cur_char_);
+      }
+      break;
+  }
 
-    cur_token_ = std::make_shared<Token>(
-        kind,
-        value,
-        source_code_.substr(start_pos, cursor_ - 1 - start_pos),
-        location);
+  cur_token_ = std::make_shared<Token>(
+      kind, value, source_code_.substr(start_pos, cursor_ - 1 - start_pos),
+      location);
 }
 
 }  // namespace bfcc
