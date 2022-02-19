a5edf4fe6a137f4ec9450561c0d73651ae91d9d0
liubang <it.liubang@gmail.com>
2022-02-19 14:25:57 +0800

    update

--- cpp/test/cpp20/test_coroutines.cpp
+++ cpp/test/cpp20/test_coroutines.cpp
@@ -1,49 +1,44 @@
 #include <coroutine>
+#include <exception>
 #include <iostream>
-#include <stdexcept>
-#include <thread>
 
-#incude < gtest / gtest.h>
-
-auto switch_to_new_thread(std::jthread& out) {
-  struct awaitable {
-    std::jthread* p_out;
-    bool await_ready() { return false; }
-    void await_suspend(std::coroutine_handle<> h) {
-      std::jthread& out = *p_out;
-      if (out.joinable()) {
-        throw std::runtime_error("Output jthread parameter not empty");
-      }
-      out = std::jthread([h] { h.resume(); });
-      std::cout << "New thread ID: " << out.get_id() << '\n';
-    }
-    void await_resume() {}
-  };
-  return awaitable(&out);
-}
-
-struct Task {
-  struct PromiseType {
-    Task get_return_object() { return {}; }
-    std::suspend_never initial_suspend() { return {}; }
+template <typename T> struct Generator {
+  struct promise_type {
+    T value;
+    std::suspend_always initial_suspend() { return {}; }
     std::suspend_never final_suspend() noexcept { return {}; }
-    void return void() {}
+    std::suspend_always yield_value(T t) {
+      value = t;
+      return {};
+    }
     void unhandled_exception() {}
+    Generator<T> get_return_object() {
+      return Generator<T>{
+          std::coroutine_handle<promise_type>::from_promise(*this)};
+    }
+    void return_void() {}
   };
+
+  std::coroutine_handle<promise_type> handle;
+
+  T next() {
+    handle.resume();
+    return handle.promise().value;
+  }
 };
 
-Task resuming_on_new_thread(std::jthread& out) {
-  std::cout << "Coroutine started on thread: " << std::this_thread::get_id()
-            << '\n';
-
-  co_await switch_to_new_thread(out);
-  std::cout << "Coroutine resumed on thread: " << std::this_thread::get_id()
-            << '\n';
+template <typename T> Generator<T> sequence() {
+  int i = 0;
+  while (true) {
+    co_yield i++;
+  }
+  co_return;
 }
 
-// TEST(cpp20, coroutines) {
 int main(int argc, char* argv[]) {
-  std::jthread out;
-  resuming_on_new_thread(out);
+  auto gen = sequence<int64_t>();
+  for (int i = 0; i < 10; ++i) {
+    std::cout << gen.next() << std::endl;
+  }
   return 0;
 }
