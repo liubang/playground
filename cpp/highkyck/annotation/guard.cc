af675765ba550c28e55103b21f3802fba0170c6a
liubang <it.liubang@gmail.com>
2022-02-28 00:04:40 +0800

    update

--- cpp/highkyck/annotation/guard.cc
+++ cpp/highkyck/annotation/guard.cc
@@ -5,19 +5,21 @@
 // Enable thread safety attributes only with clang.
 // The attributes can be safely erased when compiling with other compilers.
 #if defined(__clang__) && (!defined(SWIG))
-#define THREAD_ANNOTATION_ATTRIBUTE__(x) __attribute__((x))
+#  define THREAD_ANNOTATION_ATTRIBUTE__(x) __attribute__((x))
 #else
-#define THREAD_ANNOTATION_ATTRIBUTE__(x)  // no-op
+#  define THREAD_ANNOTATION_ATTRIBUTE__(x)  // no-op
 #endif
 
 #define GUARDED_BY(x) THREAD_ANNOTATION_ATTRIBUTE__(guarded_by(x))
 
-namespace highkyck {
-namespace annotation {
+namespace highkyck::annotation {
 
 class Foo {
  public:
-  void set(const std::string& key, const std::string& val) { map_[key] = val; }
+  void set(const std::string& key, const std::string& val) {
+    std::unique_lock<std::mutex> lk(mutex_);
+    map_[key] = val;
+  }
 
   std::string get(const std::string& key) {
     std::unique_lock<std::mutex> lk(mutex_);
@@ -29,8 +31,7 @@ class Foo {
   std::unordered_map<std::string, std::string> map_ GUARDED_BY(mutex_);
 };
 
-}  // namespace annotation
-}  // namespace highkyck
+}  // namespace highkyck::annotation
 
 int main(int argc, char* argv[]) {
   highkyck::annotation::Foo foo;
