ff6deab89b237d899d8578e61471861d4684d8c9
liubang <it.liubang@gmail.com>
2022-04-11 16:08:54 +0800

    update

---
+++ java/gradle-test/src/test/java/cn/iliubang/gradle/test/gradletest/GradleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.iliubang.gradle.test.gradletest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
