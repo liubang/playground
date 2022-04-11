8efafbbd0dbd7ad628c82e53884d4eb0557b9103
liubang <it.liubang@gmail.com>
2022-04-11 16:08:54 +0800

    update

---
+++ java/gradle-test/readme.md
@@ -0,0 +1,4 @@
+## 这是gradle测试项目
+
+- 学习gradle的使用
+- 学习spring-boot 2.5的使用
---
+++ java/gradle-test/src/main/java/cn/iliubang/gradle/test/gradletest/GradleTestApplication.java
@@ -0,0 +1,13 @@
+package cn.iliubang.gradle.test.gradletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleTestApplication.class, args);
+	}
+
+}
