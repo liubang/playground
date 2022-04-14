a4bb1e47de8d3104860181ae11cf5571d0cb294e
liubang <it.liubang@gmail.com>
2022-04-15 00:05:55 +0800

    update

--- cpp/highkyck/misk/consumer.cc
+++ cpp/highkyck/misk/consumer.cc
@@ -7,7 +7,8 @@ extern ::std::condition_variable cond;
 extern ::std::mutex mtx;
 extern ::std::queue<::std::shared_ptr<int>> queue;
 
-::std::shared_ptr<int> consume() {
+::std::shared_ptr<int> consume()
+{
   ::std::unique_lock<::std::mutex> lk(mtx);
   while (queue.empty()) cond.wait(lk);
   auto g = queue.front();
