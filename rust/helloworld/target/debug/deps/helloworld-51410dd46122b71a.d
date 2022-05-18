90b44f521295e5030aa431f7985e39fa5aacdd02
liubang <it.liubang@gmail.com>
2022-05-19 00:21:18 +0800

    update

--- rust/helloworld/target/debug/deps/helloworld-51410dd46122b71a.d
+++
@@ -1,5 +0,0 @@
-/mnt/workspace/liubang/laboratory/rust/helloworld/target/debug/deps/helloworld-51410dd46122b71a.rmeta: src/main.rs
-
-/mnt/workspace/liubang/laboratory/rust/helloworld/target/debug/deps/helloworld-51410dd46122b71a.d: src/main.rs
-
-src/main.rs:
