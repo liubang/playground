9d5013f636af657de247089ff5bae8883ce9b660
liubang <it.liubang@gmail.com>
2022-05-21 23:59:57 +0800

    update

--- cpp/highkyck/misk/test_parameters.cc
+++ cpp/highkyck/misk/test_parameters.cc
@@ -1,33 +1,38 @@
 #include <iostream>
 namespace highkyck {
-class Foo {
- public:
+class Foo
+{
+public:
   Foo() { std::cout << "构造函数" << '\n'; }
-  Foo(const Foo& foo) { std::cout << "复制构造" << '\n'; };
-  Foo(const Foo&& foo) { std::cout << "移动构造" << '\n'; }
-  Foo& operator=(const Foo& foo) {
+  Foo(const Foo &foo) { std::cout << "复制构造" << '\n'; };
+  Foo(const Foo &&foo) { std::cout << "移动构造" << '\n'; }
+  Foo &operator=(const Foo &foo)
+  {
     std::cout << "拷贝构造" << '\n';
     return *this;
   }
-  Foo& operator=(const Foo&& foo) {
+  Foo &operator=(const Foo &&foo)
+  {
     std::cout << "移动拷贝构造" << '\n';
     return *this;
   }
   ~Foo() = default;
 };
 
-class Bar {
- public:
-  Bar(const Foo& foo) : foo_(foo) {}
+class Bar
+{
+public:
+  Bar(const Foo &foo) : foo_(foo) {}
 
   Foo foo() const { return foo_; }
 
- private:
+private:
   Foo foo_;
 };
-}  // namespace highkyck
+}// namespace highkyck
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   highkyck::Foo foo;
   highkyck::Bar bar(foo);
 
--- cpp/highkyck/misk/test_singleton.cc
+++ cpp/highkyck/misk/test_singleton.cc
@@ -3,17 +3,19 @@
 #include "includes/singleton.h"
 
 namespace highkyck {
-class Foo {
- public:
+class Foo
+{
+public:
   Foo() = default;
   virtual ~Foo() = default;
 };
-}  // namespace highkyck
+}// namespace highkyck
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   highkyck::misk::Singleton<highkyck::Foo> foo;
-  highkyck::Foo* f1 = foo.getInstance();
-  highkyck::Foo* f2 = foo.getInstance();
+  highkyck::Foo *f1 = foo.getInstance();
+  highkyck::Foo *f2 = foo.getInstance();
 
   std::cout << f1 << std::endl;
   std::cout << f2 << std::endl;
