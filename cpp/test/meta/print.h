04fe5120760604e51231f0ca642709d8753c61b2
liubang <it.liubang@gmail.com>
2022-09-11 01:29:57 +0800

    update

---
+++ cpp/test/meta/print.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+
+namespace highkyck::meta {
+
+void print1() {}
+
+template<typename A, typename... Args> void print1(const A &arg, const Args &...args)
+{
+  std::cout << arg << std::endl;
+  print1(args...);
+}
+
+
+template<typename... Args> void print2(const Args &...args)
+{
+  ((std::cout << args << std::endl), ...);
+}
+
+
+}// namespace highkyck::meta
