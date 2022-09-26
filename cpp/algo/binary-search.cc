c5c9da6ddd969aad123f2eadf41156be7108fc0d
liubang <it.liubang@gmail.com>
2022-09-27 00:12:33 +0800

    update

--- cpp/algo/binary-search.cc
+++ cpp/algo/binary-search.cc
@@ -11,11 +11,9 @@
 
 namespace {
 constexpr double EPS = 1e-6;
-class EquationRoot
-{
-public:
-  double getRoot()
-  {
+class EquationRoot {
+ public:
+  double getRoot() {
     double s = 0, e = 100;
     double m = (s + e) / 2;
     double y = f(m);
@@ -31,15 +29,13 @@ public:
     return m;
   }
 
-private:
+ private:
   inline double f(double x) { return x * x * x - 5 * x * x + 10 * x - 80; }
 };
 
-class TowSum
-{
-public:
-  std::pair<int, int> find(std::vector<int>& nums, int target)
-  {
+class TowSum {
+ public:
+  std::pair<int, int> find(std::vector<int>& nums, int target) {
     std::sort(nums.begin(), nums.end());
     int i = 0, j = nums.size() - 1;
     while (i <= j) {
@@ -57,15 +53,13 @@ public:
 };
 }  // namespace
 
-TEST(algo, EquationRoot)
-{
+TEST(algo, EquationRoot) {
   EquationRoot e;
   double root = e.getRoot();
   EXPECT_NEAR(root, 5.70508593, 1e-6);
 }
 
-TEST(algo, TowSum)
-{
+TEST(algo, TowSum) {
   TowSum t;
   std::vector<int> inputs = {1, 2, 8, 4, 3, 6, 13};
   std::pair<int, int> exp(4, 8);
