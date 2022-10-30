8acd8bd2597f65b0baf4814b5d0ec6229834071b
liubang <it.liubang@gmail.com>
2022-10-31 00:10:55 +0800

    update

---
+++ react/admin/src/App.tsx
@@ -0,0 +1,44 @@
+// Copyright (c) 2022 The Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Authors: liubang (it.liubang@gmail.com)
+// Created: 2022/10/31 00:10
+
+//=====================================================================
+//
+// App.tsx -
+//
+// Created by liubang on 2022/10/30 23:58
+// Last Modified: 2022/10/30 23:58
+//
+//=====================================================================
+
+import { useState } from "react";
+import { Button } from "antd";
+import { UpCircleOutlined } from "@ant-design/icons";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      顶级组件
+      <br />
+      <UpCircleOutlined style={{ fontSize: "30px", color: "Red" }} />
+      <Button type="primary">我们的按钮</Button>
+    </div>
+  );
+}
+
+export default App;
