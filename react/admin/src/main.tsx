1cc61b525c315ca3ccaa118abd2c064df15f95b7
liubang <it.liubang@gmail.com>
2022-10-31 00:10:55 +0800

    update

---
+++ react/admin/src/main.tsx
@@ -0,0 +1,46 @@
+// Copyright (c) 2022 The Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Authors: liubang (it.liubang@gmail.com)
+// Created: 2022/10/31 00:10
+
+//=====================================================================
+//
+// main.tsx -
+//
+// Created by liubang on 2022/10/30 23:58
+// Last Modified: 2022/10/30 23:58
+//
+//=====================================================================
+
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+// 样式初始化一般放在最前
+import "reset-css";
+
+// UI 框架的样式
+
+import "@/assets/styles/global.scss";
+
+
+// 组件的样式
+
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
