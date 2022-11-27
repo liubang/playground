17496986234b009f9bdb1565eb68f43ab556b2a7
liubang <it.liubang@gmail.com>
2022-11-27 19:33:50 +0800

    update fibonacci

--- cpp/highkyck/meta/fibonacci_test.cpp
+++ cpp/highkyck/meta/fibonacci_test.cpp
@@ -1,23 +1,23 @@
+#include <iostream>
+#include <limits>
+
 #include "fibonacci.h"
 
-#include <iostream>
-
 int main(int argc, char* argv[]) {
-  std::cout << "fibonacci 1 is : "
-            << ::highkyck::detail::Fibonacci_v<1> << "\n";
-  std::cout << "fibonacci 2 is : "
-            << ::highkyck::detail::Fibonacci_v<2> << "\n";
-  std::cout << "fibonacci 3 is : "
-            << ::highkyck::detail::Fibonacci_v<3> << "\n";
-  std::cout << "fibonacci 4 is : "
-            << ::highkyck::detail::Fibonacci_v<4> << "\n";
-  std::cout << "fibonacci 5 is : "
-            << ::highkyck::detail::Fibonacci_v<5> << "\n";
-  std::cout << "fibonacci 6 is : "
-            << ::highkyck::detail::Fibonacci_v<6> << "\n";
-  std::cout << "fibonacci 100 is : "
-            << ::highkyck::detail::Fibonacci_v<100> << "\n";
-  std::cout << "fibonacci 888 is : "
-            << ::highkyck::detail::Fibonacci_v<888> << "\n";
+  std::cout << highkyck::detail::fibonacci<0> << '\n';
+  std::cout << highkyck::detail::fibonacci<1> << '\n';
+  std::cout << highkyck::detail::fibonacci<2> << '\n';
+  std::cout << highkyck::detail::fibonacci<3> << '\n';
+  std::cout << highkyck::detail::fibonacci<4> << '\n';
+  std::cout << highkyck::detail::fibonacci<5> << '\n';
+  std::cout << highkyck::detail::fibonacci<666> << '\n';
+  std::cout << highkyck::detail::fibonacci<999> << '\n';
+
+  std::cout.precision(std::numeric_limits<double>::max_digits10);
+  std::cout << highkyck::detail::golden_ratio<6> << '\n';
+  std::cout << highkyck::detail::golden_ratio<10> << '\n';
+  std::cout << highkyck::detail::golden_ratio<20> << '\n';
+  std::cout << highkyck::detail::golden_ratio<50> << '\n';
+
   return 0;
 }
