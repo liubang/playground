439aa02d8fdba54006a127136c1d62c0bdf48912
liubang <it.liubang@gmail.com>
2023-05-21 02:32:46 +0800

    update

--- cpp/test/cpp14/variable_template_test.cpp
+++
@@ -1,9 +0,0 @@
-#include "variable_template.h"
-
-int main(int argc, char* argv[]) {
-  static_assert(!test::cpp14::is_digit<'x'>, "ok");
-  static_assert(test::cpp14::is_digit<'0'>, "ok");
-  static_assert(test::cpp14::is_digit<'9'>, "ok");
-  static_assert(test::cpp14::is_digit<'5'>, "ok");
-  return 0;
-}
--- cpp/test/gtest/math.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace test_gtest {
-template <typename T>
-class Math {
- public:
-  static T add(T a, T b) { return a + b; }
-};
-}  // namespace test_gtest
