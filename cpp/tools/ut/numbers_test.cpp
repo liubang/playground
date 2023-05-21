15828dae8ee30497c3b0bd6b98db1cfe149e5de5
liubang <it.liubang@gmail.com>
2023-05-21 23:50:32 +0800

    update

---
+++ cpp/tools/ut/numbers_test.cpp
@@ -0,0 +1,21 @@
+//=====================================================================
+//
+// numbers_test.cpp -
+//
+// Created by liubang on 2023/05/21 23:45
+// Last Modified: 2023/05/21 23:45
+//
+//=====================================================================
+
+#include "cpp/tools/numbers.h"
+
+#include <gtest/gtest.h>
+
+TEST(tools, ip) {
+  EXPECT_TRUE(playground::cpp::tools::is_power_of_two(1));
+  EXPECT_TRUE(playground::cpp::tools::is_power_of_two(4));
+  EXPECT_TRUE(playground::cpp::tools::is_power_of_two(8));
+  EXPECT_TRUE(playground::cpp::tools::is_power_of_two(16));
+  EXPECT_FALSE(playground::cpp::tools::is_power_of_two(10));
+  EXPECT_FALSE(playground::cpp::tools::is_power_of_two(12));
+}
