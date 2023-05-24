c5ddcf37f82bbc3755cc04a8917092e213033d20
liubang <it.liubang@gmail.com>
2023-05-25 00:51:38 +0800

    update

---
+++ python/test/sensor_pb.py
@@ -0,0 +1,12 @@
+from proto.test.sensor_pb2 import Sensor
+
+if __name__ == "__main__":
+    with open("sensor.data", 'rb') as file:
+        content = file.read()
+        print("Retrieve Sensor object from sensor.data")
+        sensor = Sensor()
+        sensor.ParseFromString(content)
+        print(f"Sensor name: {sensor.name}")
+        print(f"Sensor temperature: {sensor.temperature}")
+        print(f"Sensor humidity: {sensor.humidity}")
+    print("Sensor door: {}".format("Open" if sensor.door else "Closed"))
