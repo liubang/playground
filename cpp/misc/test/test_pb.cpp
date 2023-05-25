acd58ca920da434d6cc207d305b19c14448f74dc
liubang <it.liubang@gmail.com>
2023-05-26 00:44:32 +0800

    update

---
+++ cpp/misc/test/test_pb.cpp
@@ -0,0 +1,20 @@
+//=====================================================================
+//
+// test_pb.cpp -
+//
+// Created by liubang on 2023/05/25 23:51
+// Last Modified: 2023/05/25 23:51
+//
+//=====================================================================
+#include <fstream>
+#include "proto/test/sensor.pb.h"
+
+int main() {
+  proto::test::Sensor sensor;
+  sensor.set_name("Laboratory");
+  sensor.set_temperature(23.4);
+  sensor.set_humidity(68);
+  sensor.set_door(proto::test::Sensor_SwitchLevel_OPEN);
+  std::ofstream ofs("sensor.data", std::ios_base::out | std::ios_base::binary);
+  sensor.SerializeToOstream(&ofs);
+}
