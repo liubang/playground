aa64af8c1ce41c5b1a964a7b89b1676c65908482
liubang <it.liubang@gmail.com>
2023-05-25 21:07:04 +0800

    update

--- python/test/sensor_pb.py
+++ python/test/sensor_pb.py
@@ -19,9 +19,14 @@
 # Created: 2023/05/25 00:51
 
 from proto.test.sensor_pb2 import Sensor
+import argparse
 
 if __name__ == "__main__":
-    with open("sensor.data", 'rb') as file:
+    parser = argparse.ArgumentParser(description='这是一个简单的程序示例')
+    parser.add_argument("-f", "--file", type=str, help='文件路径')
+    args = parser.parse_args()
+
+    with open(args.file, 'rb') as file:
         content = file.read()
         print("Retrieve Sensor object from sensor.data")
         sensor = Sensor()
@@ -29,4 +34,5 @@ if __name__ == "__main__":
         print(f"Sensor name: {sensor.name}")
         print(f"Sensor temperature: {sensor.temperature}")
         print(f"Sensor humidity: {sensor.humidity}")
+
     print("Sensor door: {}".format("Open" if sensor.door else "Closed"))
