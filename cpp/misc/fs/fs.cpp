a2d55768a5a81c83a8297a45bf8778640956408c
liubang <it.liubang@gmail.com>
2023-05-29 17:22:02 +0800

    update

---
+++ cpp/misc/fs/fs.cpp
@@ -0,0 +1,13 @@
+#include "cpp/misc/fs/fs.h"
+
+namespace playground::cpp::misc::fs {
+
+Fs::Fs() = default;
+
+Fs::~Fs() = default;
+
+FsWriter::~FsWriter() = default;
+
+FsReader::~FsReader() = default;
+
+}  // namespace playground::cpp::misc::fs
