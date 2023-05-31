48713cb9eebca7b74f6ad3af92637375a33e2dda
liubang <it.liubang@gmail.com>
2023-05-31 17:28:14 +0800

    update

---
+++ cpp/misc/sst/comparator.h
@@ -0,0 +1,25 @@
+//=====================================================================
+//
+// comparator.h -
+//
+// Created by liubang on 2023/05/31 11:29
+// Last Modified: 2023/05/31 11:29
+//
+//=====================================================================
+
+#include "cpp/tools/binary.h"
+
+namespace playground::cpp::misc::sst {
+
+class Comparator {
+public:
+  virtual ~Comparator();
+
+  [[nodiscard]] virtual const char* name() const = 0;
+
+  [[nodiscard]] virtual int compare(const tools::Binary& a, const tools::Binary& b) const = 0;
+};
+
+Comparator* bytewiseComparator();
+
+}  // namespace playground::cpp::misc::sst
