cd87f8e3168a8b4827d0f765effc46a2230075e1
liubang <it.liubang@gmail.com>
2023-06-01 15:20:12 +0800

    update

---
+++ cpp/misc/sst/options.cpp
@@ -0,0 +1,19 @@
+//=====================================================================
+//
+// options.cpp -
+//
+// Created by liubang on 2023/06/01 14:29
+// Last Modified: 2023/06/01 14:29
+//
+//=====================================================================
+
+#include "cpp/misc/sst/options.h"
+#include "cpp/misc/sst/comparator.h"
+#include "cpp/misc/sst/filter_policy.h"
+
+namespace playground::cpp::misc::sst {
+
+Options::Options()
+    : comparator(bytewiseComparator()), filter_policy(newBloomFilterPolicy(bits_per_key)) {}
+
+}  // namespace playground::cpp::misc::sst
--- cpp/misc/sst/sstable_builder.cpp
+++ cpp/misc/sst/sstable_builder.cpp
@@ -7,22 +7,38 @@
 //
 //=====================================================================
 #include "cpp/misc/sst/sstable_builder.h"
+#include "cpp/misc/sst/encoding.h"
+
+#include <zlib.h>
 
 namespace playground::cpp::misc::sst {
 
+SSTableBuilder::SSTableBuilder(const Options& options, fs::FsWriter* writer)
+    : options_(options),
+      writer_(writer),
+      data_block_(&options),
+      index_block_(&options),
+      filter_block_(options.filter_policy == nullptr
+                        ? nullptr
+                        : new FilterBlockBuilder(options.filter_policy)) {}
+
 void SSTableBuilder::add(const tools::Binary& key, const tools::Binary& value) {
+  assert(!closed_);
   if (!ok()) {
     return;
   }
 
   if (pending_index_entry_) {
     assert(data_block_.empty());
-    index_block_.add(last_key_, key);
+    std::string handle_encoding;
+    pending_handler_.encodeTo(&handle_encoding);
+    // 记录上一个block的最后一个key和上一个block的结束位置
+    index_block_.add(last_key_, handle_encoding);
     pending_index_entry_ = false;
   }
 
   if (num_entries_ > 0) {
-    assert(comparator_->compare(key, last_key_) > 0);
+    assert(options_.comparator->compare(key, last_key_) > 0);
   }
 
   if (filter_block_ != nullptr) {
@@ -34,15 +50,109 @@ void SSTableBuilder::add(const tools::Binary& key, const tools::Binary& value) {
   num_entries_++;
 
   const size_t s = data_block_.sizeEstimate();
-  if (s >= block_size_) {
+  // 达到block_size后，就写入
+  if (s >= options_.block_size) {
     flush();
   }
 }
 
 void SSTableBuilder::flush() {
+  assert(!closed_);
   if (!ok() || data_block_.empty()) {
     return;
   }
+  // 写入block
+  writeBlock(&data_block_, &pending_handler_);
+  if (ok()) {
+    // 复位标记，下次开始一个新的block
+    pending_index_entry_ = true;
+    status_ = writer_->flush();
+  }
+  if (filter_block_ != nullptr) {
+    filter_block_->startBlock(offset_);
+  }
+}
+
+void SSTableBuilder::writeBlock(BlockBuilder* builder, BlockHandle* handle) {
+  assert(ok());
+  auto raw = builder->finish();
+  // TODO(liubang): support compression
+  writeBlockRaw(raw, CompressionType::kNoCompression, handle);
+}
+
+void SSTableBuilder::writeBlockRaw(const tools::Binary& content, CompressionType type,
+                                   BlockHandle* handle) {
+  handle->setOffset(offset_);
+  handle->setSize(content.size());
+  status_ = writer_->append(content);
+  if (ok()) {
+    // crc
+    char trailer[kBlockTrailerSize];
+    trailer[0] = static_cast<unsigned char>(type);
+    uint32_t crc = crc32(0L, (const Bytef*)content.data(), content.size());
+    std::string encode_crc;
+    encodeInt(&encode_crc, crc);
+    memcpy(trailer + 1, encode_crc.data(), encode_crc.size());
+    status_ = writer_->append(tools::Binary(trailer, kBlockTrailerSize));
+    if (ok()) {
+      // 更新下一个block的offset
+      offset_ += content.size() + kBlockTrailerSize;
+    }
+  }
+}
+
+// 主要是记录sst的footer
+tools::Status SSTableBuilder::finish() {
+  assert(!closed_);
+  flush();
+  closed_ = true;
+  BlockHandle filter_block_handle, metaindex_block_handle, index_block_handle;
+
+  // 写filter block
+  if (ok() && filter_block_ != nullptr) {
+    writeBlockRaw(filter_block_->finish(), CompressionType::kNoCompression, &filter_block_handle);
+  }
+
+  // 写入metaindex block
+  if (ok()) {
+    BlockBuilder meta_index_block(&options_);
+    if (filter_block_ != nullptr) {
+      std::string key = "filter.";
+      key.append(options_.filter_policy->name());
+      std::string handle_encoding;
+      filter_block_handle.encodeTo(&handle_encoding);
+      // 记录filter block的类型和位置
+      meta_index_block.add(key, handle_encoding);
+    }
+    writeBlock(&meta_index_block, &metaindex_block_handle);
+  }
+
+  // 写入index block
+  if (ok()) {
+    // 处理边界情况
+    if (pending_index_entry_) {
+      std::string handle_encoding;
+      pending_handler_.encodeTo(&handle_encoding);
+      // 记录上一个block的最后一个key和上一个block的结束位置
+      index_block_.add(last_key_, handle_encoding);
+      pending_index_entry_ = false;
+    }
+    writeBlock(&index_block_, &index_block_handle);
+  }
+
+  // 写入footer
+  if (ok()) {
+    Footer footer;
+    footer.setMetaindexHandle(metaindex_block_handle);
+    footer.setIndexHandle(index_block_handle);
+    std::string footer_content;
+    footer.encodeTo(&footer_content);
+    status_ = writer_->append(footer_content);
+    if (ok()) {
+      offset_ += footer_content.size();
+    }
+  }
+  return status_;
 }
 
 }  // namespace playground::cpp::misc::sst
