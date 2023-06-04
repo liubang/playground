eeb9bf28513c1d49e930517923989f57854d97f0
liubang <it.liubang@gmail.com>
2023-06-04 20:10:05 +0800

    update

---
+++ cpp/meta/type_list.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstddef>
+
+namespace playground::cpp::meta {
+
+template <typename... Ts>
+struct TypeList {
+  struct IsTypeList {};
+  using type = TypeList;
+  constexpr static std::size_t size = sizeof...(Ts);
+  template <typename... T>
+  using append = TypeList<Ts..., T...>;
+  template <typename... T>
+  using prepend = TypeList<T..., Ts...>;
+  template <template <typename...> typename T>
+  using to = T<Ts...>;
+};
+
+template <typename TypeList>
+concept TL = requires {
+  typename TypeList::IsTypeList;  // 约束
+  typename TypeList::type;        // 返回值
+};
+
+}  // namespace playground::cpp::meta
