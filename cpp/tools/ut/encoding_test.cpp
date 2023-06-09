8c6b9e7835d3d775e74abcc0cc8e8c28555a0ebc
liubang <it.liubang@gmail.com>
2023-06-09 20:02:51 +0800

    update

---
+++ cpp/tools/ut/encoding_test.cpp
@@ -0,0 +1,20 @@
+//=====================================================================
+//
+// encoding_test.cpp -
+//
+// Created by liubang on 2023/06/09 19:44
+// Last Modified: 2023/06/09 19:44
+//
+//=====================================================================
+
+#include "cpp/tools/encoding.h"
+
+#include <gtest/gtest.h>
+
+class EncodingTest : public ::testing::Test {
+ protected:
+  void SetUp() override {}
+  void TearDown() override {}
+};
+
+TEST_F(EncodingTest, verify_varint) {}
