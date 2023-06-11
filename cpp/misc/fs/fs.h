8df7dcb313b43553fb7dc4aa8cfbea892ea12cf1
liubang <it.liubang@gmail.com>
2023-06-11 21:58:11 +0800

    update

--- cpp/misc/fs/fs.h
+++ cpp/misc/fs/fs.h
@@ -35,9 +35,7 @@ class FsReader {
  public:
   virtual ~FsReader();
 
-  virtual tools::Status read(uint64_t offset,
-                             size_t n,
-                             tools::Binary* result,
+  virtual tools::Status read(uint64_t offset, size_t n, tools::Binary* result,
                              char* scratch) const = 0;
 
   [[nodiscard]] virtual std::size_t size() const = 0;
