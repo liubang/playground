e2852d013e5081add973b836f63881bd64beee9b
liubang <it.liubang@gmail.com>
2023-06-18 01:27:38 +0800

    update

---
+++ cpp/misc/test/tbb/parallel_invoke.cpp
@@ -0,0 +1,32 @@
+//=====================================================================
+//
+// parallel_invoke.cpp -
+//
+// Created by liubang on 2023/06/18 00:07
+// Last Modified: 2023/06/18 00:07
+//
+//=====================================================================
+#include <tbb/parallel_invoke.h>
+
+#include <iostream>
+#include <string>
+
+void download(const std::string& file) {
+  for (int i = 0; i < 10; ++i) {
+    std::cout << "Downloading " << file << " (" << i * 10 << "%)..."
+              << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(400));
+  }
+  std::cout << "Download complete: " << file << std::endl;
+}
+
+void interact() {
+  std::string name;
+  std::cin >> name;
+  std::cout << "Hi, " << name << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+  tbb::parallel_invoke([&] { download("hello.zip"); }, [&] { interact(); });
+  return 0;
+}
