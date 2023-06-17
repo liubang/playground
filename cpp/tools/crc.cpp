65bb53c23e561c30610f48df56d63a69c464e47b
liubang <it.liubang@gmail.com>
2023-06-17 22:24:08 +0800

    update

--- cpp/tools/crc.cpp
+++ cpp/tools/crc.cpp
@@ -9,9 +9,11 @@
 
 #include "cpp/tools/crc.h"
 
+#include <boost/crc.hpp>
+
 namespace pl {
 
-uint32_t crc32(const void* data, size_t length) {
+uint32_t crc32(const void* data, std::size_t length) {
   boost::crc_32_type result;
   result.process_bytes(data, length);
   return result.checksum();
