19e7af6fb8c1446d7e990554881cb022ea91fe16
liubang <it.liubang@gmail.com>
2023-06-18 20:58:57 +0800

    update

--- cpp/misc/test/tbb/tools.h
+++ cpp/misc/test/tbb/tools.h
@@ -14,10 +14,19 @@
 
 namespace pl {
 
-template <typename Fn,
-          std::enable_if<std::is_invocable_v<typename std::decay<Fn>::type>,
-                         void>::type* = nullptr>
-void measure(Fn&& fn) {
+template <typename Fn>
+concept FuncWithVoidRet = std::is_invocable_v<std::decay_t<Fn>> &&
+                          std::is_void_v<std::invoke_result_t<Fn>>;
+
+template <typename Fn>
+concept FuncWithNonVoidRet = std::is_invocable_v<std::decay_t<Fn>> &&
+                             !std::is_void_v<std::invoke_result_t<Fn>>;
+
+template <typename Fn>
+  requires FuncWithVoidRet<Fn>
+auto measure(Fn&& fn) {
+  static_assert(std::is_invocable_v<std::decay_t<Fn>>);
+  static_assert(std::is_void_v<std::invoke_result_t<Fn>>);
   auto start = std::chrono::high_resolution_clock::now();
   fn();
   auto end = std::chrono::high_resolution_clock::now();
@@ -25,5 +34,42 @@ void measure(Fn&& fn) {
       std::chrono::duration_cast<std::chrono::microseconds>(end - start);
   std::cout << ">> Elapsed time: " << elapsed.count() << "(us)" << std::endl;
 }
+
+template <typename Fn>
+  requires FuncWithNonVoidRet<Fn>
+auto measure(Fn&& fn) {
+  static_assert(std::is_invocable_v<std::decay_t<Fn>>);
+  static_assert(!std::is_void_v<std::invoke_result_t<Fn>>);
+  auto start = std::chrono::high_resolution_clock::now();
+  auto res = fn();
+  auto end = std::chrono::high_resolution_clock::now();
+  auto elapsed =
+      std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+  std::cout << ">> Elapsed time: " << elapsed.count() << "(us)" << std::endl;
+  return res;
+}
+
+//// for c++17
+
+// template <typename Fn,
+//           std::enable_if_t<std::is_invocable_v<std::decay_t<Fn>>>* = nullptr>
+// auto measure(Fn&& fn) {
+//   auto start = std::chrono::high_resolution_clock::now();
+//   if constexpr (std::is_void_v<std::invoke_result_t<Fn>>) {
+//     fn();
+//     auto end = std::chrono::high_resolution_clock::now();
+//     auto elapsed =
+//         std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+//     std::cout << ">> Elapsed time: " << elapsed.count() << "(us)" <<
+//     std::endl;
+//   } else {
+//     auto ret = fn();
+//     auto end = std::chrono::high_resolution_clock::now();
+//     auto elapsed =
+//         std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+//     std::cout << ">> Elapsed time: " << elapsed.count() << "(us)" <<
+//     std::endl; return ret;
+//   }
+// }
 
 }  // namespace pl
