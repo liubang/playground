acff520b74d0fb36dc9e1e56a88d77685b24194c
liubang <it.liubang@gmail.com>
2023-06-26 00:44:49 +0800

    update

--- cpp/misc/test/tbb/task_arena_benchmark.cpp
+++ cpp/misc/test/tbb/task_arena_benchmark.cpp
@@ -6,6 +6,7 @@
 // Last Modified: 2023/06/18 14:06
 //
 //=====================================================================
+#include <benchmark/benchmark.h>
 #include <tbb/parallel_for.h>
 #include <tbb/task_arena.h>
 
@@ -13,17 +14,17 @@
 #include <iostream>
 #include <vector>
 
-#include "cpp/tools/measure.h"
+constexpr size_t n = 1 << 26;
 
-int main(int argc, char *argv[]) {
-  size_t n = 1 << 26;
-  pl::measure([n] {
+static void test1(benchmark::State& state) {
+  for (auto _ : state) {
     std::vector<float> a(n);
     tbb::task_arena ta;
     ta.execute([&] {
       tbb::parallel_for((size_t)0, (size_t)n,
                         [&](size_t i) { a[i] = std::sin(i); });
     });
-  });
-  return 0;
+  }
 }
+
+BENCHMARK(test1);
