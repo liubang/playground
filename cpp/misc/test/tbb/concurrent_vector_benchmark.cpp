7e3ca930654e06c21ba70ad227c315309f250e22
liubang <it.liubang@gmail.com>
2023-07-29 22:43:12 +0800

    update

--- cpp/misc/test/tbb/concurrent_vector_benchmark.cpp
+++ cpp/misc/test/tbb/concurrent_vector_benchmark.cpp
@@ -6,7 +6,7 @@
 // Last Modified: 2023/06/18 14:35
 //
 //=====================================================================
-#include <benchmark/benchmark.h>
+#include <nanobench.h>
 #include <tbb/concurrent_vector.h>
 #include <tbb/parallel_for.h>
 
@@ -15,35 +15,35 @@
 
 constexpr size_t n = 1 << 27;
 
-static void test1(benchmark::State& state) {
-  for (auto _ : state) {
-    tbb::concurrent_vector<float> a;
-    tbb::parallel_for(tbb::blocked_range<size_t>(0, n),
-                      [&](tbb::blocked_range<size_t> r) {
-                        for (size_t i = r.begin(); i < r.end(); ++i) {
-                          float val = std::sin(i);
-                          if (val > 0) a.push_back(val);
-                        }
-                      });
-  }
+static void test1() {
+  tbb::concurrent_vector<float> a;
+  tbb::parallel_for(tbb::blocked_range<size_t>(0, n),
+                    [&](tbb::blocked_range<size_t> r) {
+                      for (size_t i = r.begin(); i < r.end(); ++i) {
+                        float val = std::sin(i);
+                        if (val > 0) a.push_back(val);
+                      }
+                    });
 }
 
-static void test2(benchmark::State& state) {
-  for (auto _ : state) {
-    tbb::concurrent_vector<float> a;
-    tbb::parallel_for(tbb::blocked_range<size_t>(0, n),
-                      [&](tbb::blocked_range<size_t> r) {
-                        std::vector<float> local_a;
-                        for (size_t i = r.begin(); i < r.end(); ++i) {
-                          float val = std::sin(i);
-                          if (val > 0) local_a.push_back(val);
-                        }
+static void test2() {
+  tbb::concurrent_vector<float> a;
+  tbb::parallel_for(tbb::blocked_range<size_t>(0, n),
+                    [&](tbb::blocked_range<size_t> r) {
+                      std::vector<float> local_a;
+                      for (size_t i = r.begin(); i < r.end(); ++i) {
+                        float val = std::sin(i);
+                        if (val > 0) local_a.push_back(val);
+                      }
 
-                        auto it = a.grow_by(local_a.size());
-                        std::copy(local_a.begin(), local_a.end(), it);
-                      });
-  }
+                      auto it = a.grow_by(local_a.size());
+                      std::copy(local_a.begin(), local_a.end(), it);
+                    });
 }
 
-BENCHMARK(test1);
-BENCHMARK(test2);
+int main(int argc, char* argv[]) {
+  // put your code here
+  ankerl::nanobench::Bench().run("test1", [&] { test1(); });
+  ankerl::nanobench::Bench().run("test2", [&] { test2(); });
+  return 0;
+}
