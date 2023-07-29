4672e2180f33f331e4a8709e5ba4e2620d75c7ac
liubang <it.liubang@gmail.com>
2023-07-29 22:43:12 +0800

    update

--- cpp/misc/test/tbb/parallel_pipeline_benchmark.cpp
+++ cpp/misc/test/tbb/parallel_pipeline_benchmark.cpp
@@ -7,7 +7,7 @@
 //
 //=====================================================================
 
-#include <benchmark/benchmark.h>
+#include <nanobench.h>
 #include <tbb/parallel_for_each.h>
 #include <tbb/parallel_pipeline.h>
 
@@ -54,85 +54,82 @@ struct Data {
 
 constexpr size_t n = 1 << 12;
 
-static void test1(benchmark::State& state) {
-  for (auto _ : state) {
-    std::vector<Data> datas(n);
-    for (auto& data : datas) {
-      data.step1();
-      data.step2();
-      data.step3();
-      data.step4();
-    }
+static void test1() {
+  std::vector<Data> datas(n);
+  for (auto& data : datas) {
+    data.step1();
+    data.step2();
+    data.step3();
+    data.step4();
   }
 }
 
-static void test2(benchmark::State& state) {
-  for (auto _ : state) {
-    std::vector<Data> datas(n);
-    tbb::parallel_for_each(datas.begin(), datas.end(), [&](Data& data) {
-      data.step1();
-      data.step2();
-      data.step3();
-      data.step4();
-    });
-  }
+static void test2() {
+  std::vector<Data> datas(n);
+  tbb::parallel_for_each(datas.begin(), datas.end(), [&](Data& data) {
+    data.step1();
+    data.step2();
+    data.step3();
+    data.step4();
+  });
 }
 
-static void test3(benchmark::State& state) {
-  for (auto _ : state) {
-    std::vector<Data> datas(n);
-    tbb::parallel_for_each(datas.begin(), datas.end(),
-                           [&](Data& data) { data.step1(); });
+static void test3() {
+  std::vector<Data> datas(n);
+  tbb::parallel_for_each(datas.begin(), datas.end(),
+                         [&](Data& data) { data.step1(); });
 
-    tbb::parallel_for_each(datas.begin(), datas.end(),
-                           [&](Data& data) { data.step2(); });
+  tbb::parallel_for_each(datas.begin(), datas.end(),
+                         [&](Data& data) { data.step2(); });
 
-    tbb::parallel_for_each(datas.begin(), datas.end(),
-                           [&](Data& data) { data.step3(); });
+  tbb::parallel_for_each(datas.begin(), datas.end(),
+                         [&](Data& data) { data.step3(); });
 
-    tbb::parallel_for_each(datas.begin(), datas.end(),
-                           [&](Data& data) { data.step4(); });
-  }
+  tbb::parallel_for_each(datas.begin(), datas.end(),
+                         [&](Data& data) { data.step4(); });
 }
 
-static void test4(benchmark::State& state) {
-  for (auto _ : state) {
-    std::vector<Data> datas(n);
-    auto it = datas.begin();
-    tbb::parallel_pipeline(
-        16,
-        tbb::make_filter<void, Data*>(tbb::filter_mode::serial_in_order,
-                                      [&](tbb::flow_control& fc) -> Data* {
-                                        if (it == datas.end()) {
-                                          fc.stop();
-                                          return nullptr;
-                                        }
-                                        return &*it++;
-                                      }),
-        tbb::make_filter<Data*, Data*>(tbb::filter_mode::parallel,
-                                       [&](Data* data) -> Data* {
-                                         data->step1();
-                                         return data;
-                                       }),
-        tbb::make_filter<Data*, Data*>(tbb::filter_mode::parallel,
-                                       [&](Data* data) -> Data* {
-                                         data->step2();
-                                         return data;
-                                       }),
-        tbb::make_filter<Data*, Data*>(tbb::filter_mode::parallel,
-                                       [&](Data* data) -> Data* {
-                                         data->step3();
-                                         return data;
-                                       }),
-        tbb::make_filter<Data*, void>(tbb::filter_mode::parallel,
-                                      [&](Data* data) -> Data* {
-                                        data->step4();
-                                        return data;
-                                      }));
-  }
+static void test4() {
+  std::vector<Data> datas(n);
+  auto it = datas.begin();
+  tbb::parallel_pipeline(
+      16,
+      tbb::make_filter<void, Data*>(tbb::filter_mode::serial_in_order,
+                                    [&](tbb::flow_control& fc) -> Data* {
+                                      if (it == datas.end()) {
+                                        fc.stop();
+                                        return nullptr;
+                                      }
+                                      return &*it++;
+                                    }),
+      tbb::make_filter<Data*, Data*>(tbb::filter_mode::parallel,
+                                     [&](Data* data) -> Data* {
+                                       data->step1();
+                                       return data;
+                                     }),
+      tbb::make_filter<Data*, Data*>(tbb::filter_mode::parallel,
+                                     [&](Data* data) -> Data* {
+                                       data->step2();
+                                       return data;
+                                     }),
+      tbb::make_filter<Data*, Data*>(tbb::filter_mode::parallel,
+                                     [&](Data* data) -> Data* {
+                                       data->step3();
+                                       return data;
+                                     }),
+      tbb::make_filter<Data*, void>(tbb::filter_mode::parallel,
+                                    [&](Data* data) -> Data* {
+                                      data->step4();
+                                      return data;
+                                    }));
 }
 
-BENCHMARK(test1);
-BENCHMARK(test2);
-BENCHMARK(test3);
-BENCHMARK(test4);
+int main(int argc, char* argv[]) {
+  // put your code here
+  ankerl::nanobench::Bench().run("test1", [&] { test1(); });
+  ankerl::nanobench::Bench().run("test2", [&] { test2(); });
+  ankerl::nanobench::Bench().run("test3", [&] { test3(); });
+  ankerl::nanobench::Bench().run("test4", [&] { test4(); });
+
+  return 0;
+}
--- cpp/misc/test/tbb/partitioner_benchmark.cpp
+++ cpp/misc/test/tbb/partitioner_benchmark.cpp
@@ -6,7 +6,7 @@
 // Last Modified: 2023/06/18 14:20
 //
 //=====================================================================
-#include <benchmark/benchmark.h>
+#include <nanobench.h>
 #include <tbb/blocked_range2d.h>
 #include <tbb/parallel_for.h>
 
@@ -17,40 +17,39 @@
 
 constexpr size_t n = 1 << 14;
 
-static void test1(benchmark::State& state) {
-  for (auto _ : state) {
-    std::vector<float> a(n * n);
-    std::vector<float> b(n * n);
+static void test1() {
+  std::vector<float> a(n * n);
+  std::vector<float> b(n * n);
 
-    tbb::parallel_for(
-        tbb::blocked_range2d<size_t>(0, n, 0, n),
-        [&](tbb::blocked_range2d<size_t> r) {
-          for (size_t i = r.cols().begin(); i < r.cols().end(); ++i) {
-            for (size_t j = r.rows().begin(); j < r.rows().end(); ++j) {
-              b[i * n + j] = a[j * n + i];
-            }
+  tbb::parallel_for(
+      tbb::blocked_range2d<size_t>(0, n, 0, n),
+      [&](tbb::blocked_range2d<size_t> r) {
+        for (size_t i = r.cols().begin(); i < r.cols().end(); ++i) {
+          for (size_t j = r.rows().begin(); j < r.rows().end(); ++j) {
+            b[i * n + j] = a[j * n + i];
           }
-        });
-  }
+        }
+      });
 }
 
-static void test2(benchmark::State& state) {
-  for (auto _ : state) {
-    std::vector<float> a(n * n);
-    std::vector<float> b(n * n);
-    size_t grain = 24;
-    tbb::parallel_for(
-        tbb::blocked_range2d<size_t>(0, n, grain, 0, n, grain),
-        [&](tbb::blocked_range2d<size_t> r) {
-          for (size_t i = r.cols().begin(); i < r.cols().end(); ++i) {
-            for (size_t j = r.rows().begin(); j < r.rows().end(); ++j) {
-              b[i * n + j] = a[j * n + i];
-            }
+static void test2() {
+  std::vector<float> a(n * n);
+  std::vector<float> b(n * n);
+  size_t grain = 24;
+  tbb::parallel_for(
+      tbb::blocked_range2d<size_t>(0, n, grain, 0, n, grain),
+      [&](tbb::blocked_range2d<size_t> r) {
+        for (size_t i = r.cols().begin(); i < r.cols().end(); ++i) {
+          for (size_t j = r.rows().begin(); j < r.rows().end(); ++j) {
+            b[i * n + j] = a[j * n + i];
           }
-        },
-        tbb::simple_partitioner{});
-  }
+        }
+      },
+      tbb::simple_partitioner{});
 }
 
-BENCHMARK(test1);
-BENCHMARK(test2);
+int main(int argc, char *argv[]) {
+  ankerl::nanobench::Bench().run("test1", [&] { test1(); });
+  ankerl::nanobench::Bench().run("test2", [&] { test2(); });
+  return 0;
+}
