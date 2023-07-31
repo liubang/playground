61bf08eff1b4e579d712f27e80bb373bbb13ee69
liubang <it.liubang@gmail.com>
2023-07-31 13:47:38 +0800

    update

--- cpp/meta/ut/tuple_iteration_apply_test.cpp
+++ cpp/meta/ut/tuple_iteration_apply_test.cpp
@@ -8,21 +8,20 @@
 //=====================================================================
 #include "cpp/meta/tuple_iteration_apply.h"
 
-#include <gtest/gtest.h>
-
+#include <catch2/catch_test_macros.hpp>
 #include <tuple>
 #include <utility>
 
-TEST(meta, PrintTupleApplyFn) {
+TEST_CASE("meta", "[PrintTupleApplyFn]") {
   std::tuple tp{10, 20, "hello"};
   pl::PrintTupleApplyFn(tp);
   std::cout << std::endl;
-  EXPECT_TRUE(true);
-}
+  REQUIRE(true);
+};
 
-TEST(meta, PrintTupleApply) {
+TEST_CASE("meta", "[PrintTupleApply]") {
   std::tuple tp{10, 20, 3.14, 42, "hello"};
   pl::PrintTupleApply(tp);
   std::cout << std::endl;
-  EXPECT_TRUE(true);
-}
+  REQUIRE(true);
+};
