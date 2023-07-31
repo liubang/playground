b41d6523ac6457e08ccc760e32807b9e3d0a9687
liubang <it.liubang@gmail.com>
2023-07-31 13:47:38 +0800

    update

--- cpp/meta/ut/tuple_iteration_basics_test.cpp
+++ cpp/meta/ut/tuple_iteration_basics_test.cpp
@@ -9,44 +9,38 @@
 
 #include "cpp/meta/tuple_iteration_basics.h"
 
-#include <gtest/gtest.h>
-
+#include <catch2/catch_test_macros.hpp>
 #include <tuple>
 #include <utility>
 
-TEST(meta, PrintTupleManual) {
+TEST_CASE("meta", "[PrintTupleManual]") {
   std::tuple tp{10, 20, "hello"};
   pl::PrintTupleManual<decltype(tp), 0, 1, 2>(tp);
   std::cout << std::endl;
-  EXPECT_TRUE(true);
-}
+};
 
-TEST(meta, PrintTupleManualEx) {
+TEST_CASE("meta", "[PrintTupleManualEx]") {
   std::tuple tp{10, 20, "hello"};
   pl::PrintTupleManualEx(tp, std::index_sequence<0, 1, 2>{});
   std::cout << std::endl;
   pl::PrintTupleManualEx(
       tp, std::make_index_sequence<std::tuple_size_v<decltype(tp)>>{});
   std::cout << std::endl;
-  EXPECT_TRUE(true);
-}
+};
 
-TEST(meta, PrintTupleAutoGetSize) {
+TEST_CASE("meta", "[PrintTupleAutoGetSize]") {
   std::tuple tp{10, 20, "hello"};
   pl::PrintTupleAutoGetSize(tp);
   std::cout << std::endl;
-  EXPECT_TRUE(true);
-}
+};
 
-TEST(meta, PrintTupleFinal) {
+TEST_CASE("meta", "[PrintTupleFinal]") {
   std::tuple tp{10, 20, "hello"};
   pl::PrintTupleFinal(tp);
   std::cout << std::endl;
-  EXPECT_TRUE(true);
-}
+};
 
-TEST(meta, PrintTupleWithOstream) {
+TEST_CASE("meta", "[PrintTupleWithOstream]") {
   std::tuple tp{10, 20, "hello"};
   std::cout << tp << std::endl;
-  EXPECT_TRUE(true);
-}
+};
