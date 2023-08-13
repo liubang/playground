3bf39edd0c199b6b394b89bbc3dd20ec122e6df1
liubang <it.liubang@gmail.com>
2023-08-14 00:46:55 +0800

    update

---
+++ cpp/misc/test/google_benchmark/fibonacci.cpp
@@ -0,0 +1,16 @@
+#include <benchmark/benchmark.h>
+
+#include <cstdint>
+
+std::uint64_t Fibonacci(std::uint64_t number) {
+  return number < 2 ? 1 : Fibonacci(number - 1) + Fibonacci(number - 2);
+}
+
+static void BM_StringCreation(benchmark::State& state) {
+  for (auto _ : state) {
+    Fibonacci(20);
+  }
+}
+
+// Register the function as a benchmark
+BENCHMARK(BM_StringCreation);
