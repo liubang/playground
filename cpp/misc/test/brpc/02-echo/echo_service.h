fc98b3367308bdc0c6e20dcf98e807a3c6827607
liubang <it.liubang@gmail.com>
2023-08-18 01:27:35 +0800

    update

---
+++ cpp/misc/test/brpc/02-echo/echo_service.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "cpp/misc/test/brpc/02-echo/echo.pb.h"
+
+namespace pl::brpc {
+class EchoServiceImpl : public EchoService {
+ public:
+  void Echo(::google::protobuf::RpcController* cntl_base,
+            const ::pl::brpc::EchoRequest* request,
+            ::pl::brpc::EchoResponse* response,
+            ::google::protobuf::Closure* done) override;
+};
+}  // namespace pl::brpc
