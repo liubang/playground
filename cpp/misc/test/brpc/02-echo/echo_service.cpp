288abc2ae09cffde2adcc62ada2aaf3f22912cbf
liubang <it.liubang@gmail.com>
2023-08-18 01:27:35 +0800

    update

---
+++ cpp/misc/test/brpc/02-echo/echo_service.cpp
@@ -0,0 +1,20 @@
+//=====================================================================
+//
+// echo_service.cpp -
+//
+// Created by liubang on 2023/08/18 01:01
+// Last Modified: 2023/08/18 01:01
+//
+//=====================================================================
+#include "echo_service.h"
+
+#include <brpc/controller.h>
+
+void pl::brpc::EchoServiceImpl::Echo(
+    ::google::protobuf::RpcController* cntl_base,
+    const ::pl::brpc::EchoRequest* request, ::pl::brpc::EchoResponse* response,
+    ::google::protobuf::Closure* done) {
+  ::brpc::ClosureGuard done_guard(done);
+  [[__maybe_unused__]] auto* cntl = static_cast<::brpc::Controller*>(cntl_base);
+  response->set_message(request->message());
+}
