254957b8caaac0c8144ab39b2801ba422263f533
liubang <it.liubang@gmail.com>
2023-08-22 11:19:50 +0800

    update

---
+++ cpp/misc/test/lemon/calc1/ttokens.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// clang-format off
+#define TK_PLUS    1
+#define TK_MINUS   2
+#define TK_TIMES   3
+#define TK_DIVIDE  4
+#define TK_LPAR    5
+#define TK_RPAR    6
+#define TK_INTEGER 7
+// clang-format on
