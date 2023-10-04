fb00c9f8d607a405731edff20e545b4b207f392e
liubang <it.liubang@gmail.com>
2023-10-05 01:33:24 +0800

    update

---
+++ cpp/features/cpp17/test_pmr1.cpp
@@ -0,0 +1,78 @@
+//=====================================================================
+//
+// test_pmr.cpp -
+//
+// Created by liubang on 2023/10/04 19:43
+// Last Modified: 2023/10/04 19:43
+//
+//=====================================================================
+#include <cstdio>
+#include <list>
+#include <vector>
+
+#include "cpp/tools/measure.h"
+
+static char g_buf[65535 * 30];
+
+struct my_mem_resource {
+    size_t m_watermark = 0;
+    char* m_buf = g_buf;
+
+    char* do_allocate(size_t n, size_t align) {
+        m_watermark = (m_watermark + align - 1) / align * align;
+        char* p = m_buf + m_watermark;
+        m_watermark += n;
+        return p;
+    }
+};
+
+static my_mem_resource* g_mem_resource = new my_mem_resource;
+
+template <typename T> struct my_allocator {
+    using value_type = T;
+
+    my_mem_resource* m_resource{nullptr};
+
+    my_allocator() : m_resource(g_mem_resource) {}
+    my_allocator(my_mem_resource* m_resource) : m_resource(m_resource) {}
+
+    template <typename U> constexpr my_allocator(my_allocator<U> const& other) noexcept {}
+    constexpr bool operator==(my_allocator const& other) const noexcept { return this == &other; }
+
+    T* allocate(size_t n) {
+        char* p = m_resource->do_allocate(n * sizeof(T), alignof(T));
+        return (T*)p;
+    }
+
+    void deallocate(T* p, size_t n) {}
+};
+
+int main(int argc, char* argv[]) {
+    pl::measure("vector", []() {
+        std::vector<char> a;
+        a.reserve(65535);
+        for (int i = 0; i < 65535; ++i) {
+            a.push_back(42);
+        }
+    });
+
+    // ===============================================================
+
+    pl::measure("list1", []() {
+        std::list<char> a;
+        for (int i = 0; i < 65535; ++i) {
+            a.push_back(42);
+        }
+    });
+
+    // ===============================================================
+
+    pl::measure("list2", []() {
+        std::list<char, my_allocator<char>> a;
+        for (int i = 0; i < 65535; ++i) {
+            a.push_back(42);
+        }
+    });
+
+    return 0;
+}
---
+++ cpp/misc/fsha/local_fs.h
@@ -0,0 +1,39 @@
+//=====================================================================
+//
+// local_fs.h -
+//
+// Created by liubang on 2023/10/01 22:12
+// Last Modified: 2023/10/01 22:12
+//
+//=====================================================================
+#pragma once
+
+#include "cpp/misc/fsha/fs_adaptor.h"
+
+namespace pl {
+
+class LocalFile : public FileAdaptor {
+public:
+    LocalFile(int fd, const std::string& name, ssize_t size)
+        : FileAdaptor(name), fd_(fd), size_(size) {}
+    virtual ~LocalFile() = default;
+
+    ssize_t size() const override;
+    ssize_t append(const void* buf, const size_t count) override;
+    ssize_t read(void* buf, const size_t count, int64_t offset, bool* is_eof) override;
+    int clear() override;
+    int close() override;
+    int sync() override;
+
+private:
+private:
+    int fd_;
+    ssize_t size_;
+    bool direct_io_{false};
+};
+
+class LocalDir : public DirAdaptor {};
+
+class LocalFs : public FsAdaptor {};
+
+} // namespace pl
