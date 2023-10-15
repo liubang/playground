10a9295e22fc208e409e8bab85cc134200f8c877
liubang <it.liubang@gmail.com>
2023-10-15 13:08:19 +0800

    update

---
+++ cpp/features/cpp17/test_visit.cpp
@@ -0,0 +1,71 @@
+//=====================================================================
+//
+// test_visit.cpp -
+//
+// Created by liubang on 2023/10/15 12:46
+// Last Modified: 2023/10/15 12:46
+//
+//=====================================================================
+
+#include <iostream>
+#include <variant>
+#include <vector>
+
+std::variant<std::false_type, std::true_type> bool_variant(bool x) {
+    if (x) {
+        return std::true_type{};
+    }
+    return std::false_type{};
+}
+
+void saxpy(std::vector<float> a, float b, float c) {
+    auto has_b = bool_variant(b != 1);
+    auto has_c = bool_variant(b != 0);
+    std::visit(
+        [&](auto has_b, auto has_c) {
+            for (auto& ai : a) {
+                if constexpr (has_b) {
+                    ai *= b;
+                }
+                if constexpr (has_c) {
+                    ai += c;
+                }
+            }
+        },
+        has_b, has_c);
+}
+
+int main(int argc, char* argv[]) {
+    // auto func = [&](auto val) {
+    //     static int _ = printf("instanced!!!\n");
+    //     std::cout << val << std::endl;
+    // };
+    //
+    // func(std::integral_constant<int, 1>{});
+    // func(std::integral_constant<int, 2>{});
+    // func(std::integral_constant<int, 3>{});
+
+    // std::variant<std::false_type, std::true_type> v;
+    // v = std::false_type{};
+    // v = std::true_type{};
+    //
+    // std::visit(
+    //     [&](auto val) {
+    //         static int _ = printf("instanced!!!!\n");
+    //         if constexpr (val) {
+    //             std::cout << "true" << std::endl;
+    //         } else {
+    //             std::cout << "false" << std::endl;
+    //         }
+    //     },
+    //     v);
+    //
+
+    std::vector<float> a(1024);
+    saxpy(a, 1, 0);
+    saxpy(a, 1, 3.14);
+    saxpy(a, 2, 0);
+    saxpy(a, 2, 3.14);
+
+    return 0;
+}
