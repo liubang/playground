da17a5d46fe0f87e738189b332d61440a39543a7
liubang <it.liubang@gmail.com>
2023-10-30 21:37:26 +0800

    update

---
+++ cpp/misc/test/yacc/flux/main.c
@@ -0,0 +1,16 @@
+//=====================================================================
+//
+// main.c -
+//
+// Created by liubang on 2023/10/30 20:03
+// Last Modified: 2023/10/30 20:03
+//
+//=====================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    printf("hello world\n");
+    return EXIT_SUCCESS;
+}
