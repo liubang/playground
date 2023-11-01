689129f259991736c8578c02742138cb37695c12
liubang <it.liubang@gmail.com>
2023-11-02 01:12:27 +0800

    update

--- cpp/misc/flux/scanner.cpp
+++ cpp/misc/flux/scanner.cpp
@@ -8,6 +8,7 @@
 //=====================================================================
 #include <cstring>
 #include <map>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -16,10 +17,14 @@
 struct Position {
     uint32_t line;
     uint32_t column;
+
+    Position() = default;
+    Position(uint32_t line, uint32_t column) : line(line), column(column) {}
 };
 
 struct Comment {
     std::string text;
+    Comment() = default;
     Comment(std::string text) : text(std::move(text)) {}
 };
 
@@ -28,44 +33,62 @@ struct Token {
     std::string lit;
     uint32_t start_offset;
     uint32_t end_offset;
-    Position start_pos;
-    Position end_pos;
-    std::vector<Comment> comments;
+    std::shared_ptr<Position> start_pos;
+    std::shared_ptr<Position> end_pos;
+    std::vector<std::shared_ptr<Comment>> comments;
 };
+
+// TODO(liubang): to implement.
+extern int real_scan(const char* data,
+                     int32_t mode,
+                     const char* p,
+                     const char* ps,
+                     const char* pe,
+                     const char* eof,
+                     const char*& last_newline,
+                     int32_t& cur_line,
+                     TokenType& token,
+                     int32_t& token_start,
+                     int32_t& token_start_line,
+                     int32_t& token_start_col,
+                     int32_t& token_end,
+                     int32_t& token_end_line,
+                     int32_t& token_end_col);
 
 class Scanner {
 public:
     Scanner(const char* data, size_t len)
         : data_(data),
-          ps_(0),
-          p_(0),
-          pe_(len),
-          eof_(len),
-          last_newline_(0),
+          data_len_(len),
+          ps_(data),
+          p_(data),
+          pe_(data + len),
+          eof_(data + len),
+          last_newline_(data),
           cur_line_(1),
-          checkpoint_(0),
+          checkpoint_(data),
           checkpoint_line_(1),
-          checkpoint_last_newline_(0),
+          checkpoint_last_newline_(data),
           token_(TokenType::Illegal) {}
 
 public:
-    Token scan() { return scan_with_comments(0); }
+    std::shared_ptr<Token> scan() { return scan_with_comments(0); }
 
-    Token scan_with_comments(int32_t mode) {
-        Token token;
+    std::shared_ptr<Token> scan_with_comments(int32_t mode) {
+        std::shared_ptr<Token> token;
         for (;;) {
             token = scan(mode);
-            if (token.tok != TokenType::Comment) {
+            if (token->tok != TokenType::Comment) {
                 break;
             }
-            comments_.emplace_back(token.lit);
+            comments_.emplace_back(std::make_shared<Comment>(token->lit));
         }
-        token.comments.insert(token.comments.end(), comments_.begin(), comments_.end());
+        token->comments.insert(token->comments.end(), comments_.begin(), comments_.end());
         return token;
     }
 
-    Token scan_with_regex(int32_t mode) { return scan_with_comments(1); }
-    Token scan_with_expr(int32_t mode) { return scan_with_comments(2); }
+    std::shared_ptr<Token> scan_with_regex() { return scan_with_comments(1); }
+    std::shared_ptr<Token> scan_with_expr() { return scan_with_comments(2); }
 
     /**
      * unread will reset the Scanner to go back to the location before the last scan_with_regex or
@@ -81,7 +104,7 @@ public:
     /**
      * Get the offset of a position
      */
-    uint32_t offset(const Position& pos) {
+    uint32_t offset(std::shared_ptr<Position> pos) {
         if (positions_.count(pos) == 0) {
             // TODO(liubang): error handler
             return UINT32_MAX;
@@ -92,12 +115,12 @@ public:
     /**
      * Append a comment to the current scanner
      */
-    void set_comments(const std::vector<Comment>& t) {
+    void set_comments(const std::vector<std::shared_ptr<Comment>>& t) {
         comments_.insert(comments_.end(), t.begin(), t.end());
     }
 
 private:
-    Token scan(int32_t mode) {
+    std::shared_ptr<Token> scan(int32_t mode) {
         if (p_ == eof_) {
             return get_eof_token();
         }
@@ -112,79 +135,54 @@ private:
         int32_t token_end_line = 0;
         int32_t token_end_col = 0;
 
-        auto err =
-            scan(data_, mode, p_, ps_, pe_, eof_, last_newline_, cur_line_, token_, token_start,
-                 token_start_line, token_start_col, token_end, token_end_line, token_end_col);
-        Token t;
+        auto err = real_scan(data_, mode, p_, ps_, pe_, eof_, last_newline_, cur_line_, token_,
+                             token_start, token_start_line, token_start_col, token_end,
+                             token_end_line, token_end_col);
+        std::shared_ptr<Token> t;
         if (err != 0) {
+            // TODO(liubang):
         }
         if (token_ == TokenType::Illegal && p_ == eof_) {
             t = get_eof_token();
         } else {
-            t = Token{.tok = token_,
-                      .lit = std::string(data_ + token_start, token_end),
-                      .start_offset = static_cast<uint32_t>(token_start),
-                      .end_offset = static_cast<uint32_t>(token_end),
-                      .start_pos =
-                          Position{
-                              .line = static_cast<uint32_t>(token_start_line),
-                              .column = static_cast<uint32_t>(token_start_col),
-                          },
-                      .end_pos = Position{
-                          .line = static_cast<uint32_t>(token_end_line),
-                          .column = static_cast<uint32_t>(token_end_col),
-                      }};
+            t->tok = token_;
+            t->lit = std::string(data_ + token_start, token_end);
+            t->start_offset = token_start;
+            t->end_offset = token_end;
+            t->start_pos = std::make_shared<Position>(token_start_line, token_start_col);
+            t->end_pos = std::make_shared<Position>(token_end_line, token_end_col);
         }
-        positions_[t.start_pos] = t.start_offset;
-        positions_[t.end_pos] = t.end_offset;
+        positions_[t->start_pos] = t->start_offset;
+        positions_[t->end_pos] = t->end_offset;
 
         return t;
     }
 
-    Token get_eof_token() {
-        uint32_t data_len = ::strlen(data_);
+    std::shared_ptr<Token> get_eof_token() {
         uint32_t column = eof_ - last_newline_ + 1;
-        return Token{
-            .tok = TokenType::Eof,
-            .lit = "",
-            .start_offset = data_len,
-            .end_offset = data_len,
-            .start_pos = Position{.line = static_cast<uint32_t>(cur_line_), .column = column},
-            .end_pos = Position{.line = static_cast<uint32_t>(cur_line_), .column = column},
-        };
-    }
-
-    // TODO(liubang): to implement.
-    int scan(const char* data,
-             int32_t mode,
-             int32_t& p,
-             int32_t ps,
-             int32_t pe,
-             int32_t eof,
-             int32_t& last_newline,
-             int32_t& cur_line,
-             TokenType& token,
-             int32_t& token_start,
-             int32_t& token_start_line,
-             int32_t& token_start_col,
-             int32_t& token_end,
-             int32_t& token_end_line,
-             int32_t& token_end_col) {
-        return 0;
+        auto token = std::make_shared<Token>();
+        token->tok = TokenType::Eof;
+        token->lit = "";
+        token->start_offset = data_len_;
+        token->end_offset = data_len_;
+        token->start_pos = std::make_shared<Position>(cur_line_, column);
+        token->end_pos = std::make_shared<Position>(cur_line_, column);
+        return token;
     }
 
 private:
     const char* data_;
-    int32_t ps_;
-    int32_t p_;
-    int32_t pe_;
-    int32_t eof_;
-    int32_t last_newline_;
+    size_t data_len_;
+    const char* ps_;
+    const char* p_;
+    const char* pe_;
+    const char* eof_;
+    const char* last_newline_;
     int32_t cur_line_;
-    int32_t checkpoint_;
+    const char* checkpoint_;
     int32_t checkpoint_line_;
-    int32_t checkpoint_last_newline_;
+    const char* checkpoint_last_newline_;
     TokenType token_;
-    std::map<Position, uint32_t> positions_;
-    std::vector<Comment> comments_;
+    std::map<std::shared_ptr<Position>, uint32_t> positions_;
+    std::vector<std::shared_ptr<Comment>> comments_;
 };
