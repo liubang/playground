92f1173451329367410797a9b4f89c2330f5733a
liubang <it.liubang@gmail.com>
2023-11-02 12:20:11 +0800

    update

--- cpp/misc/flux/token.h
+++ cpp/misc/flux/token.h
@@ -8,6 +8,10 @@
 //=====================================================================
 #pragma once
 
+#include <ostream>
+#include <string>
+
+namespace pl {
 enum class TokenType {
     Illegal,
     Eof,
@@ -75,3 +79,155 @@ enum class TokenType {
 
     Attribute,
 };
+
+inline std::string token_to_string(TokenType token) {
+    switch (token) {
+    case TokenType::Illegal:
+        return "Illegal";
+    case TokenType::Eof:
+        return "Eof";
+    case TokenType::Comment:
+        return "Comment";
+    case TokenType::And:
+        return "And";
+    case TokenType::Or:
+        return "Or";
+    case TokenType::Not:
+        return "Not";
+    case TokenType::Import:
+        return "Import";
+    case TokenType::Package:
+        return "Package";
+    case TokenType::Return:
+        return "Return";
+    case TokenType::Option:
+        return "Option";
+    case TokenType::Builtin:
+        return "Builtin";
+    case TokenType::TestCase:
+        return "TestCase";
+    case TokenType::If:
+        return "If";
+    case TokenType::Then:
+        return "Then";
+    case TokenType::Else:
+        return "Else";
+    case TokenType::Ident:
+        return "Ident";
+    case TokenType::Int:
+        return "Int";
+    case TokenType::Float:
+        return "Float";
+    case TokenType::String:
+        return "String";
+    case TokenType::Regex:
+        return "Regex";
+    case TokenType::Time:
+        return "Time";
+    case TokenType::Duration:
+        return "Duration";
+    case TokenType::Add:
+        return "Add";
+    case TokenType::Sub:
+        return "Sub";
+    case TokenType::Mul:
+        return "Mul";
+    case TokenType::Div:
+        return "Div";
+    case TokenType::Mod:
+        return "Mod";
+    case TokenType::Pow:
+        return "Pow";
+    case TokenType::Eq:
+        return "Eq";
+    case TokenType::Lt:
+        return "Lt";
+    case TokenType::Gt:
+        return "Gt";
+    case TokenType::Lte:
+        return "Lte";
+    case TokenType::Gte:
+        return "Gte";
+    case TokenType::Neq:
+        return "Neq";
+    case TokenType::RegexEq:
+        return "RegexEq";
+    case TokenType::RegexNeq:
+        return "RegexNeq";
+    case TokenType::Assign:
+        return "Assign";
+    case TokenType::Arrow:
+        return "Arrow";
+    case TokenType::LParen:
+        return "LParen";
+    case TokenType::RParen:
+        return "RParen";
+    case TokenType::LBrack:
+        return "LBrack";
+    case TokenType::RBrack:
+        return "RBrack";
+    case TokenType::LBrace:
+        return "LBrack";
+    case TokenType::RBrace:
+        return "RBrace";
+    case TokenType::Comma:
+        return "Comma";
+    case TokenType::Dot:
+        return "Dot";
+    case TokenType::Colon:
+        return "Colon";
+    case TokenType::PipeForward:
+        return "PipeForward";
+    case TokenType::PipeReceive:
+        return "PipeReceive";
+    case TokenType::Exists:
+        return "Exists";
+    case TokenType::Quote:
+        return "Quote";
+    case TokenType::StringExpr:
+        return "StringExpr";
+    case TokenType::Text:
+        return "Text";
+    case TokenType::QuestionMark:
+        return "QuestionMark";
+    case TokenType::Attribute:
+        return "Attribute";
+    default:
+        return "Illegal";
+    }
+}
+
+struct Position {
+    uint32_t line;
+    uint32_t column;
+
+    Position() = default;
+    Position(uint32_t line, uint32_t column) : line(line), column(column) {}
+};
+
+struct Comment {
+    std::string text;
+    Comment() = default;
+    Comment(std::string text) : text(std::move(text)) {}
+};
+
+struct Token {
+    TokenType tok;
+    std::string lit;
+    uint32_t start_offset;
+    uint32_t end_offset;
+    std::shared_ptr<Position> start_pos;
+    std::shared_ptr<Position> end_pos;
+    std::vector<std::shared_ptr<Comment>> comments;
+};
+
+inline std::ostream& operator<<(std::ostream& os, const Token& token) {
+    os << "{ tok: " << token_to_string(token.tok) << ", lit: " << token.lit << ", offset: ["
+       << token.start_offset << ", " << token.end_offset
+       << "], start_pos: {line: " << token.start_pos->line
+       << ", column: " << token.start_pos->column << "}, end_pos: {line: " << token.end_pos->line
+       << ", column: " << token.end_pos->column << "}}";
+    return os;
+}
+
+} // namespace pl
