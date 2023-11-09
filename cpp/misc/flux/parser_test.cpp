2cf56d85b7fa40a2d2b8a38f9f50963912b374e1
liubang <it.liubang@gmail.com>
2023-11-09 16:53:50 +0800

    update

---
+++ cpp/misc/flux/parser_test.cpp
@@ -0,0 +1,40 @@
+//=====================================================================
+//
+// parser_test.cpp -
+//
+// Created by liubang on 2023/11/09 16:22
+// Last Modified: 2023/11/09 16:22
+//
+//=====================================================================
+#include <iostream>
+#include <memory>
+
+#include "parser.h"
+
+int main(int argc, char* argv[]) {
+    std::string flux = R"(
+    import "array"
+    import "math"
+    import "influxdata/influxdb/sample"
+
+    from(bucket:"telegraf/autogen")
+        |> range(start:-1h)
+        |> filter(fn:(r) =>
+            r._measurement == "cpu" and
+            r.cpu == "cpu-total"
+        )
+        |> aggregateWindow(every: 1m, fn: mean)
+    )";
+
+    pl::Parser parser(flux);
+    auto file = parser.parse_file("");
+
+    std::cout << file->name << "\n";
+    std::cout << file->body.size() << "\n";
+
+    for (const auto& stmt : file->body) {
+        std::cout << static_cast<int>(stmt->type) << "\n";
+    }
+
+    return 0;
+}
