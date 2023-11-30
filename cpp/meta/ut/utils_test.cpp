384715692771a03f8cfdc21266c71e757e144c1b
liubang <it.liubang@gmail.com>
2023-11-30 23:09:34 +0800

    update

--- cpp/meta/ut/utils_test.cpp
+++ cpp/meta/ut/utils_test.cpp
@@ -14,52 +14,49 @@
 #include "cpp/meta/ut/utils_test.h"
 
 TEST_CASE("utils", "[array]") {
-  using arr345 = std::array<std::array<std::array<int, 3>, 4>, 5>;
-  // 这里的纬度是逆序的
-  static_assert(pl::is_same_v<arr345, pl::Array<int, 5, 4, 3>::type>);
-  pl::print(1, 2, 3, 4, 5);
+    using arr345 = std::array<std::array<std::array<int, 3>, 4>, 5>;
+    // 这里的纬度是逆序的
+    static_assert(pl::is_same_v<arr345, pl::Array<int, 5, 4, 3>::type>);
+    pl::print(1, 2, 3, 4, 5);
 };
 
 TEST_CASE("utils", "[map]") {
-  using longList = pl::TypeList<char, float, int, double, char>;
-  static_assert(
-      std::is_same_v<pl::Map<longList, std::add_pointer>::type,
-                     pl::TypeList<char*, float*, int*, double*, char*>>);
+    using longList = pl::TypeList<char, float, int, double, char>;
+    static_assert(std::is_same_v<pl::Map<longList, std::add_pointer>::type,
+                                 pl::TypeList<char*, float*, int*, double*, char*>>);
 };
 
 TEST_CASE("utils", "[filter]") {
-  using longList = pl::TypeList<char, float, int, double, char>;
+    using longList = pl::TypeList<char, float, int, double, char>;
 
-  static_assert(std::is_same_v<pl::Filter<longList, pl::sizeLess4>::type,
-                               pl::TypeList<char, char>>);
+    static_assert(
+        std::is_same_v<pl::Filter<longList, pl::sizeLess4>::type, pl::TypeList<char, char>>);
 };
 
 TEST_CASE("utils", "[fold]") {
-  using longList = pl::TypeList<char, float, int, double, char>;
-  static_assert(pl::Fold<longList, std::integral_constant<size_t, 0>,
-                         pl::TypeSizeAcc>::type::value == 18);
+    using longList = pl::TypeList<char, float, int, double, char>;
+    static_assert(
+        pl::Fold<longList, std::integral_constant<size_t, 0>, pl::TypeSizeAcc>::type::value == 18);
 };
 
 TEST_CASE("utils", "[concat]") {
-  static_assert(
-      std::is_same_v<
-          pl::Concat_t<pl::TypeList<int, double>, pl::TypeList<char, float>>,
-          pl::TypeList<int, double, char, float>>);
+    static_assert(std::is_same_v<pl::Concat_t<pl::TypeList<int, double>, pl::TypeList<char, float>>,
+                                 pl::TypeList<int, double, char, float>>);
 };
 
 TEST_CASE("utils", "[elem]") {
-  using longList = pl::TypeList<char, float, int, double, char>;
-  static_assert(pl::Elem<longList, char>::value);
-  static_assert(!pl::Elem<longList, std::string>::value);
+    using longList = pl::TypeList<char, float, int, double, char>;
+    static_assert(pl::Elem<longList, char>::value);
+    static_assert(!pl::Elem<longList, std::string>::value);
 };
 
 TEST_CASE("utils", "[unique]") {
-  using longList = pl::TypeList<char, float, int, double, char>;
-  static_assert(std::is_same_v<pl::Unique<longList>::type,
-                               pl::TypeList<char, float, int, double>::type>);
+    using longList = pl::TypeList<char, float, int, double, char>;
+    static_assert(
+        std::is_same_v<pl::Unique<longList>::type, pl::TypeList<char, float, int, double>::type>);
 };
 
 TEST_CASE("utils", "[sums]") {
-  //
-  static_assert(pl::sums(1, 2, 3, 4) == 10);
+    //
+    static_assert(pl::sums(1, 2, 3, 4) == 10);
 };
