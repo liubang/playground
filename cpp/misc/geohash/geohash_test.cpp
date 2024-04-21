18e55ab4e3968f7e39152d09434a38942ae02e9b
liubang <it.liubang@gmail.com>
2024-04-22 00:10:42 +0800

    update

--- cpp/misc/geohash/geohash_test.cpp
+++ cpp/misc/geohash/geohash_test.cpp
@@ -14,27 +14,15 @@
 
 // Authors: liubang (it.liubang@gmail.com)
 
+#include "geo.h"
 #include "geohash.h"
 
 #include <gtest/gtest.h>
 
-std::string hash_tostring(uint64_t bits) {
-    constexpr std::string_view alphabets = "0123456789bcdefghjkmnpqrstuvwxyz";
-    char buf[12];
-    for (int i = 0; i < 11; ++i) {
-        int idx = 0;
-        if (i != 10) {
-            idx = (bits >> (52 - ((i + 1) * 5))) & 0x1f;
-        }
-        buf[i] = alphabets[idx];
-    }
-    buf[11] = '\0';
-    return buf;
-}
-
 TEST(geohash, encode) {
     std::map<std::string, pl::GeoHash::Point> cases = {
-        {"wx4ey9n3gm0", pl::GeoHash::Point{116.31, 40.04}}};
+        {"wx4ey9n3gk0", pl::GeoHash::Point{116.31, 40.04}}};
+
     constexpr pl::GeoHash::Rectangle range = {
         pl::GeoHash::Point{-180, -90},
         pl::GeoHash::Point{180, 90},
@@ -43,12 +31,6 @@ TEST(geohash, encode) {
     for (const auto& [k, v] : cases) {
         pl::GeoHash::HashBits hash;
         pl::GeoHash::encode(range, v.lng, v.lat, 26, &hash);
-
-        // WHY?
-        // the hash in redis is: 4068802680076238
-        // but 4068802680076235 here
-        ::printf("=============%lu\n", hash.bits);
-
-        EXPECT_EQ(k, hash_tostring(hash.bits));
+        EXPECT_EQ(k, pl::Geo::geohash_string(hash));
     }
 }
