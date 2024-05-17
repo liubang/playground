ef724858e33cfd7fde032c5148499b3190f12fe6
liubang <it.liubang@gmail.com>
2024-05-17 14:03:05 +0800

    update

--- cpp/misc/z3/zn.h
+++ cpp/misc/z3/zn.h
@@ -20,6 +20,7 @@
 
 #include <cmath>
 #include <cstdint>
+#include <functional>
 #include <vector>
 
 namespace pl::curve {
@@ -28,23 +29,85 @@ template <int32_t Dims, int32_t BitsPerDim, int32_t TotalBits, uint64_t MaxMask>
 public:
     virtual ~ZN() = default;
 
+    /**
+     * @brief Insert (Dims - 1) zeros between each bit to create a zvalue from a single dimension.
+     * Only the first BitsPerDim can be considered
+     *
+     * @param value value to split
+     * @return
+     */
     [[nodiscard]] virtual uint64_t split(uint64_t value) const = 0;
 
+    /**
+     * @brief Conbine every (Dims - 1) bits to re-create a single dimension. Opposite of split.
+     *
+     * @param z value to combine
+     * @return
+     */
     [[nodiscard]] virtual uint64_t combine(uint64_t z) const = 0;
 
+    /**
+     * @brief Is the value contained in the range. Considers user-space
+     *
+     * @param range
+     * @param value
+     * @return
+     */
     [[nodiscard]] virtual bool contains(const Zrange& range, uint64_t value) const = 0;
 
+    /**
+     * @brief Is the value contained in the range. Considers user-space
+     *
+     * @param range
+     * @param value
+     * @return
+     */
     [[nodiscard]] bool contains(const Zrange& range, const Zrange& value) const {
         return contains(range, value.min) && contains(range, value.max);
     }
 
-    [[nodiscard]] virtual bool overlap(const Zrange& range, const Zrange& value) const = 0;
+    /**
+     * @brief Does the value overlap with the range. Considers user-space
+     *
+     * @param range
+     * @param value
+     * @return
+     */
+    [[nodiscard]] virtual bool overlaps(const Zrange& range, const Zrange& value) const = 0;
+
+    /**
+     * @brief Returns (litmax, bigmin) for the given range and point
+     *
+     * @param p point
+     * @param rmin min value
+     * @param rmax max value
+     * @return (litmax, bigmin)
+     */
+    [[nodiscard]] std::pair<uint64_t, uint64_t> zdivide(uint64_t p,
+                                                        uint64_t rmin,
+                                                        uint64_t rmax) const {
+        // TODO:
+        return {0, 0};
+    }
 
     void zranges(const std::vector<Zrange>& zbounds,
                  int32_t precision,
                  int32_t max_ranges,
                  int32_t max_recurse,
-                 std::vector<IndexRange>* idx_ranges) {}
+                 std::vector<IndexRange>* idx_ranges) {
+        // TODO:
+    }
+
+protected:
+    using LoadFunc = std::function<uint64_t(uint64_t, uint64_t, uint32_t, uint32_t)>;
+
+    [[nodiscard]] std::pair<uint64_t, uint64_t> zdiv(
+        LoadFunc&& load, uint32_t dims, uint64_t xd, uint64_t rmin, uint64_t rmax) const {
+        assert(rmin < rmax);
+        // TODO:
+        auto zmin = rmin;
+        auto zmax = rmax;
+    }
 
 protected:
     static constexpr int32_t DEFAULT_RECURSE = 7;
