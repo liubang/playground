97cc2195f6414edcc81431a7f3d9937f785637ee
liubang <it.liubang@gmail.com>
2024-05-29 20:30:47 +0800

    update

--- cpp/misc/sst/ut/table_test.cpp
+++ cpp/misc/sst/ut/table_test.cpp
@@ -1,11 +1,18 @@
-//=====================================================================
+// Copyright (c) 2024 The Authors. All rights reserved.
 //
-// table_test.cpp -
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// Created by liubang on 2023/06/04 17:11
-// Last Modified: 2023/06/04 17:11
+//      https://www.apache.org/licenses/LICENSE-2.0
 //
-//=====================================================================
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Authors: liubang (it.liubang@gmail.com)
 
 #include "cpp/misc/fs/fs.h"
 #include "cpp/misc/sst/table.h"
@@ -13,7 +20,6 @@
 
 #include <gtest/gtest.h>
 #include <iostream>
-// #include "absl/cleanup/cleanup.h"
 
 TEST(table, table) {
     auto* options = new pl::Options();
@@ -33,14 +39,6 @@ TEST(table, table) {
         delete reader;
         delete table;
     };
-
-    // absl::Cleanup cleanup = [&]() {
-    //   delete options->comparator;
-    //   delete options->filter_policy;
-    //   delete options;
-    //   delete reader;
-    //   delete table;
-    // };
 
     constexpr int COUNT = 10001;
 
