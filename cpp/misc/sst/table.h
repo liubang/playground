9287c15a24ad51827e0431c1803c490e4a27bfe7
liubang <it.liubang@gmail.com>
2024-05-29 20:30:47 +0800

    update

--- cpp/misc/sst/table.h
+++ cpp/misc/sst/table.h
@@ -1,11 +1,19 @@
-//=====================================================================
+// Copyright (c) 2024 The Authors. All rights reserved.
 //
-// table.h -
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// Created by liubang on 2023/06/04 00:36
-// Last Modified: 2023/06/04 00:36
+//      https://www.apache.org/licenses/LICENSE-2.0
 //
-//=====================================================================
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Authors: liubang (it.liubang@gmail.com)
+
 #pragma once
 
 #include "cpp/misc/fs/fs.h"
@@ -41,9 +49,9 @@ private:
     FsReader* reader_;
     Status status_;
     FilterBlockReader* filter_{nullptr};
-    const char* filter_data_;
+    const char* filter_data_{nullptr};
     // BlockHandle metaindex_Handle_;
-    Block* index_block_;
+    Block* index_block_{nullptr};
 };
 
 } // namespace pl
