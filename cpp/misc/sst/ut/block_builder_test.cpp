70c6dd2ab63174e5a4065de4afd2f2c3a684e2d7
liubang <it.liubang@gmail.com>
2024-05-31 17:40:35 +0800

    update

--- cpp/misc/sst/ut/block_builder_test.cpp
+++ cpp/misc/sst/ut/block_builder_test.cpp
@@ -16,6 +16,7 @@
 
 #include "cpp/misc/sst/block.h"
 #include "cpp/misc/sst/block_builder.h"
+#include "cpp/misc/sst/encoding.h"
 #include "cpp/tools/random.h"
 #include "cpp/tools/scope.h"
 
@@ -23,16 +24,22 @@
 #include <unordered_map>
 #include <vector>
 
-TEST(block_builder, test) {
+namespace pl {
+
+class BlockBuilderTest : public ::testing::Test {
+    void SetUp() override {}
+    void TearDown() override {}
+};
+
+TEST_F(BlockBuilderTest, test) {
     auto* options = new pl::Options();
+    options->block_restart_interval = 16;
 
     SCOPE_EXIT {
         delete options->comparator;
         delete options->filter_policy;
         delete options;
     };
-
-    options->block_restart_interval = 16;
 
     pl::BlockBuilder block_builder(options);
     constexpr int COUNT = 10001;
@@ -68,7 +75,6 @@ TEST(block_builder, test) {
     pl::Block b(block_content);
 
     auto* itr = b.iterator(nullptr);
-    // absl::Cleanup cleanup1 = [&]() { delete itr; };
 
     SCOPE_EXIT { delete itr; };
 
@@ -87,47 +93,45 @@ TEST(block_builder, test) {
     }
 
     // try to parse
-    // const char* data = block.data();
-    // std::size_t size = block.size();
-    // auto restart_count =
-    // pl::decodeInt<uint32_t>(&data[size - 4]);
-    // EXPECT_EQ(restart_count, (COUNT / 16) + (COUNT % 16 == 0 ? 0 : 1));
-    //
-    // // parse all restarts
-    // std::vector<uint32_t> restarts(restart_count + 1);
-    // for (int i = 0; i < restart_count; ++i) {
-    //   uint32_t offset = 4 * (i + 2);
-    //   auto restart = pl::decodeInt<uint32_t>(&data[size
-    //   - offset]); restarts[i] = restart;
-    // }
-    //
-    // restarts[restart_count] = size - (4 * (restart_count + 1));
-    //
-    // std::string pre_key;
-    // // parse keys and values
-    // for (int i = 0; i < restart_count; ++i) {
-    //   // 计算每一个restart的起始位置和结束位置
-    //   uint32_t start = restarts[i];
-    //   uint32_t end = restarts[i + 1];
-    //   // 解析每一个restart中的key和value
-    //   while (start < end) {
-    //     assert((start + 12) < end);
-    //     auto shared = pl::decodeInt<uint32_t>(data +
-    //     start); auto non_shared =
-    //     pl::decodeInt<uint32_t>(data + start + 4); auto
-    //     value_size = pl::decodeInt<uint32_t>(data +
-    //     start + 8); assert(start + 12 + non_shared + value_size <= end);
-    //
-    //     assert(pre_key.size() >= shared);
-    //     std::string key(pre_key.data(), shared);
-    //     key.append(data + start + 12, non_shared);
-    //     pre_key = key;
-    //     std::string value(data + start + 12 + non_shared, value_size);
-    //
-    //     EXPECT_TRUE(kvs.contains(key));
-    //     EXPECT_TRUE(kvs[key] == value);
-    //
-    //     start += 12 + non_shared + value_size;
-    //   }
-    // }
+    const char* data = block.data();
+    std::size_t size = block.size();
+    auto restart_count = pl::decodeInt<uint32_t>(&data[size - 4]);
+    EXPECT_EQ(restart_count, (COUNT / 16) + (COUNT % 16 == 0 ? 0 : 1));
+
+    // parse all restarts
+    std::vector<uint32_t> restarts(restart_count + 1);
+    for (int i = 0; i < restart_count; ++i) {
+        uint32_t offset = 4 * (i + 2);
+        auto restart = pl::decodeInt<uint32_t>(&data[size - offset]);
+        restarts[i] = restart;
+    }
+    restarts[restart_count] = size - (4 * (restart_count + 1));
+    std::string pre_key;
+    // parse keys and values
+    for (int i = 0; i < restart_count; ++i) {
+        // 计算每一个restart的起始位置和结束位置
+        uint32_t start = restarts[i];
+        uint32_t end = restarts[i + 1];
+        // 解析每一个restart中的key和value
+        while (start < end) {
+            assert((start + 12) < end);
+            auto shared = pl::decodeInt<uint32_t>(data + start);
+            auto non_shared = pl::decodeInt<uint32_t>(data + start + 4);
+            auto value_size = pl::decodeInt<uint32_t>(data + start + 8);
+            assert(start + 12 + non_shared + value_size <= end);
+
+            assert(pre_key.size() >= shared);
+            std::string key(pre_key.data(), shared);
+            key.append(data + start + 12, non_shared);
+            pre_key = key;
+            std::string value(data + start + 12 + non_shared, value_size);
+
+            EXPECT_TRUE(kvs.contains(key));
+            EXPECT_TRUE(kvs[key] == value);
+
+            start += 12 + non_shared + value_size;
+        }
+    }
 }
+
+} // namespace pl
