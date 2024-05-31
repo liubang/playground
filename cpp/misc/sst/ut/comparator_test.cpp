1052a008b36b5b7ee89392eaeb3d29c9e7a68b96
liubang <it.liubang@gmail.com>
2024-05-31 17:40:35 +0800

    update

--- cpp/misc/sst/ut/comparator_test.cpp
+++ cpp/misc/sst/ut/comparator_test.cpp
@@ -1,18 +1,33 @@
-//=====================================================================
+// Copyright (c) 2024 The Authors. All rights reserved.
 //
-// comparator_test.cpp -
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// Created by liubang on 2023/05/31 14:32
-// Last Modified: 2023/05/31 14:32
+//      https://www.apache.org/licenses/LICENSE-2.0
 //
-//=====================================================================
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Authors: liubang (it.liubang@gmail.com)
 
 #include "cpp/misc/sst/comparator.h"
 
 #include <gtest/gtest.h>
 #include <memory>
 
-TEST(comparator, compara) {
+namespace pl {
+
+class ComparatorTest : public ::testing::Test {
+
+    void SetUp() override {}
+    void TearDown() override {}
+};
+
+TEST_F(ComparatorTest, compara) {
     auto* comparator = pl::bytewiseComparator();
     std::unique_ptr<pl::Comparator> comparator_ptr(comparator);
 
@@ -30,3 +45,5 @@ TEST(comparator, compara) {
     EXPECT_TRUE(comparator_ptr->compare(b1, b5) > 0);
     EXPECT_TRUE(comparator_ptr->compare(b1, b6) < 0);
 }
+
+} // namespace pl
