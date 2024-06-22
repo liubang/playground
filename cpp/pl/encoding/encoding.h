2879a88d094858470be685ec9cb88e82a2d8c1d9
liubang <it.liubang@gmail.com>
2024-06-23 00:47:23 +0800

    update

--- cpp/pl/encoding/encoding.h
+++ cpp/pl/encoding/encoding.h
@@ -16,8 +16,9 @@
 
 #pragma once
 
-#include <algorithm>
+#include <cassert>
 #include <cstdint>
+#include <string>
 #include <type_traits>
 
 namespace pl {
@@ -25,49 +26,55 @@ namespace pl {
 template <typename T> struct is_varint_type : std::false_type {};
 
 template <> struct is_varint_type<uint16_t> : std::true_type {};
-
 template <> struct is_varint_type<uint32_t> : std::true_type {};
-
 template <> struct is_varint_type<uint64_t> : std::true_type {};
 
-template <typename T, std::enable_if_t<is_varint_type<T>::type>* = nullptr>
-inline void* varint_encode(T value, void* const buffer, std::size_t buffer_size) {
+template <typename T, std::enable_if_t<is_varint_type<T>::value>* = nullptr>
+bool varint_encode(T value, std::string* dist) {
     static_assert(sizeof(T) <= 8);
-    auto min_size = std::min(buffer_size, sizeof(T));
-    auto* p = static_cast<uint8_t*>(buffer);
-    uint64_t count = 0;
-    for (; count < min_size && value >= 0x7F; ++count) {
+    assert(dist != nullptr);
+
+    constexpr auto size = sizeof(T);
+    char buffer[size];
+    auto* p = reinterpret_cast<uint8_t*>(buffer);
+    size_t count = 0;
+    for (; count < size && value >= 0x7F; ++count) {
         // (value & 127) | 128;
         *(p++) = ((value & 0x7F) | 0x80);
         value >>= 7;
     }
-    if (count == buffer_size) {
-        return nullptr;
+
+    if (count == size) {
+        return false;
     }
 
-    return p;
+    dist->append(buffer, count);
+
+    return true;
 }
 
 template <typename T, std::enable_if_t<is_varint_type<T>::value>* = nullptr>
-inline void* varint_decode(void* const buffer, std::size_t buffer_size, T* result) {
+bool varint_decode(std::string_view buffer, T* result) {
     static_assert(sizeof(T) <= 8);
     constexpr auto final_byte_mask = static_cast<uint8_t>(~(uint32_t(1) << sizeof(T)) - 1);
-    auto min_size = std::min(buffer_size, sizeof(T));
-    auto* p = static_cast<uint8_t*>(buffer);
+    constexpr auto size = sizeof(T);
+    const auto* p = reinterpret_cast<const uint8_t*>(buffer.data());
     T value = 0;
     uint64_t count = 0;
     uint64_t shift = 0;
-    for (; count < min_size && (*p && 0x80 != 0); ++count) {
+    for (; count < size && (*p && 0x80 != 0); ++count) {
         value |= static_cast<T>((*p++ & 0x7F) << shift);
         shift += 7;
     }
-    if (count == buffer_size) {
-        return nullptr;
+    if (count == size) {
+        return false;
     }
     if (count == sizeof(T) && (*p & final_byte_mask) != 0) {
-        return nullptr;
+        return false;
     }
+
     *result = value;
+    return true;
 }
 
 } // namespace pl
