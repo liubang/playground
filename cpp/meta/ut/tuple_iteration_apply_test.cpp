5febd9994866d66bb9029a94315d6d110f8c2ce4
liubang <it.liubang@gmail.com>
2024-06-23 21:00:14 +0800

    update

--- cpp/meta/ut/tuple_iteration_apply_test.cpp
+++ cpp/meta/ut/tuple_iteration_apply_test.cpp
@@ -1,16 +1,23 @@
-//=====================================================================
+// Copyright (c) 2024 The Authors. All rights reserved.
 //
-// tuple_iteration_apply_test.cpp -
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// Created by liubang on 2023/05/21 23:23
-// Last Modified: 2023/05/21 23:23
+//      https://www.apache.org/licenses/LICENSE-2.0
 //
-//=====================================================================
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Authors: liubang (it.liubang@gmail.com)
+
 #include "cpp/meta/tuple_iteration_apply.h"
 
 #include <catch2/catch_test_macros.hpp>
 #include <tuple>
-#include <utility>
 
 TEST_CASE("meta", "[PrintTupleApplyFn]") {
     std::tuple tp{10, 20, "hello"};
--- cpp/pl/test/tbb/parallel_sort_benchmark.cpp
+++ cpp/pl/test/tbb/parallel_sort_benchmark.cpp
@@ -1,16 +1,23 @@
-//=====================================================================
+// Copyright (c) 2024 The Authors. All rights reserved.
 //
-// parallel_sort.cpp -
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// Created by liubang on 2023/06/18 17:08
-// Last Modified: 2023/06/18 17:08
+//      https://www.apache.org/licenses/LICENSE-2.0
 //
-//=====================================================================
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Authors: liubang (it.liubang@gmail.com)
+
 #include <nanobench.h>
 #include <tbb/parallel_sort.h>
 
 #include <algorithm>
-#include <iostream>
 #include <vector>
 
 constexpr size_t n = 1 << 24;
