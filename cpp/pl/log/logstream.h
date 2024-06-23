52a7544a9bec1f77b8c0e0af01d33c43499bfea6
liubang <it.liubang@gmail.com>
2024-06-24 01:07:31 +0800

    update

---
+++ cpp/pl/log/logstream.h
@@ -0,0 +1,92 @@
+// Copyright (c) 2024 The Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Authors: liubang (it.liubang@gmail.com)
+
+#pragma once
+
+#include <cstddef>
+#include <cstring>
+#include <string_view>
+
+namespace pl {
+
+namespace detail {
+
+template <std::size_t SIZE> class FixedBuffer {
+public:
+    FixedBuffer(const FixedBuffer&) = delete;
+    FixedBuffer(FixedBuffer&&) = delete;
+    FixedBuffer& operator=(const FixedBuffer&) = delete;
+    FixedBuffer& operator=(FixedBuffer&&) = delete;
+
+    FixedBuffer() : cursor_(data_) {}
+    ~FixedBuffer() = default;
+
+    void append(std::string_view buf) {
+        if (avail() > buf.size()) {
+            std::memcpy(cursor_, buf.data(), buf.size());
+            cursor_ += buf.size();
+        }
+    }
+
+    [[nodiscard]] const char* data() const { return data_; }
+    [[nodiscard]] const char* current() const { return cursor_; }
+    [[nodiscard]] std::size_t size() const { return cursor_ - data_; }
+    [[nodiscard]] std::size_t avail() const { return end() - cursor_; }
+
+    void reset() { cursor_ = data_; }
+
+private:
+    [[nodiscard]] const char* end() const { return data_ + sizeof(data_); }
+
+private:
+    char data_[SIZE];
+    char* cursor_;
+};
+
+} // namespace detail
+
+class LogStream {
+    using self = LogStream;
+    using Buffer = detail::FixedBuffer<4000>;
+
+public:
+    // noncopyable and nonmoveable
+    LogStream(const LogStream&) = delete;
+    LogStream(LogStream&&) = delete;
+    LogStream& operator=(const LogStream&) = delete;
+    LogStream& operator=(LogStream&&) = delete;
+
+    self& operator<<(bool v);
+    self& operator<<(short v);
+    self& operator<<(unsigned short v);
+    self& operator<<(int v);
+    self& operator<<(unsigned int v);
+    self& operator<<(long v);
+    self& operator<<(unsigned long v);
+    self& operator<<(long long v);
+    self& operator<<(unsigned long long v);
+    self& operator<<(float v);
+    self& operator<<(char v);
+    self& operator<<(const char* v);
+    self& operator<<(const unsigned char* v);
+    self& operator<<(const std::string& v);
+    self& operator<<(std::string_view v);
+
+private:
+    Buffer buffer_;
+};
+
+} // namespace pl
