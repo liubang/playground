ff45dd0b08a20931f4b8fb2137738700f0afcd0f
liubang <it.liubang@gmail.com>
2024-06-25 17:55:36 +0800

    update

--- cpp/pl/log/logger.cpp
+++ cpp/pl/log/logger.cpp
@@ -15,5 +15,36 @@
 // Authors: liubang (it.liubang@gmail.com)
 
 #include "cpp/pl/log/logger.h"
+#include "cpp/pl/thread/thread.h"
 
-namespace pl {}
+#include <fmt/chrono.h>
+#include <fmt/format.h>
+#include <iostream>
+#include <string_view>
+
+namespace pl {
+
+Logger::Logger(Logger::SourceFile source, int line) : impl_(source, line, LogLevel::PL_INFO) {}
+
+Logger::Logger(Logger::SourceFile source, int line, Logger::LogLevel log_level)
+    : impl_(source, line, log_level) {}
+
+Logger::~Logger() {
+    const auto& buffer = stream().buffer();
+    std::cout << std::string_view(buffer.data(), buffer.size()) << std::endl;
+}
+
+std::string Logger::Impl::fmtTime() {
+    auto now_time_t = std::chrono::system_clock::to_time_t(time_);
+    std::tm now_tm = *std::localtime(&now_time_t);
+    auto duration = time_.time_since_epoch();
+    auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(duration) % 1000;
+    return fmt::format("{:%m-%d %H:%M:%S}:{:03}", now_tm, millis.count());
+}
+
+void Logger::Impl::startSession() {
+    stream_ << logLevel2String(log_level_) << ": " << fmtTime() << ": * " << gettid() << " ["
+            << source_file_.source() << ":" << line_ << "] ";
+}
+
+} // namespace pl
