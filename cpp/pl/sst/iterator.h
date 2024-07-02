24b1b3063521a77af2f80029296495c30fc524e0
liubang <it.liubang@gmail.com>
2024-07-02 22:39:13 +0800

    update

--- cpp/pl/sst/iterator.h
+++ cpp/pl/sst/iterator.h
@@ -17,15 +17,12 @@
 
 #pragma once
 
+#include "cpp/pl/sst/cell.h"
 #include "cpp/pl/status/status.h"
 
 #include <memory>
 
 namespace pl {
-
-class Iterator;
-
-using IteratorPtr = std::unique_ptr<Iterator>;
 
 class Iterator {
 public:
@@ -48,9 +45,13 @@ public:
 
     [[nodiscard]] virtual bool valid() const = 0;
 
+    [[nodiscard]] virtual CellPtr cell() const = 0;
+
     [[nodiscard]] virtual std::string_view key() const = 0;
 
     [[nodiscard]] virtual std::string_view val() const = 0;
 };
+
+using IteratorPtr = std::unique_ptr<Iterator>;
 
 } // namespace pl
