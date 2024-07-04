05fec0f1e6d55fcdae72d135cb929a7a256a7a0d
liubang <it.liubang@gmail.com>
2024-07-04 16:30:45 +0800

    update

--- cpp/pl/sst/comparator.h
+++ cpp/pl/sst/comparator.h
@@ -17,6 +17,7 @@
 #pragma once
 
 #include <memory>
+#include <string>
 #include <string_view>
 
 namespace pl {
@@ -28,6 +29,10 @@ public:
     [[nodiscard]] virtual const char* name() const = 0;
 
     [[nodiscard]] virtual int compare(std::string_view a, std::string_view b) const = 0;
+
+    virtual void findShortestSeparator(std::string* start, std::string_view limit) const = 0;
+
+    virtual void findShortSucessor(std::string* key) const = 0;
 };
 
 using ComparatorPtr = std::unique_ptr<Comparator>;
@@ -41,6 +46,38 @@ public:
 
     [[nodiscard]] int compare(std::string_view a, std::string_view b) const override {
         return a.compare(b);
+    }
+
+    // TODO(liubang): use these method to reduce internal structure space usage
+    void findShortestSeparator(std::string* start, std::string_view limit) const override {
+        size_t min_length = std::min(start->size(), limit.size());
+        size_t diff_index = 0;
+        while ((diff_index < min_length) && ((*start)[diff_index] == limit[diff_index])) {
+            ++diff_index;
+        }
+        if (diff_index == min_length) {
+            // do nothing
+            return;
+        }
+        uint8_t diff_byte = static_cast<uint8_t>((*start)[diff_index]);
+        // start: hello , limit: heln => helm
+        if (diff_byte < static_cast<uint8_t>(0xff) &&
+            diff_byte + 1 < static_cast<uint8_t>(limit[diff_index])) {
+            (*start)[diff_index]++;
+            start->resize(diff_index + 1);
+        }
+    }
+
+    void findShortSucessor(std::string* key) const override {
+        size_t n = key->size();
+        for (size_t i = 0; i < n; ++i) {
+            const uint8_t byte = static_cast<uint8_t>((*key)[i]);
+            if (byte != static_cast<uint8_t>(0xff)) {
+                (*key)[i]++;
+                key->resize(i + 1);
+                return;
+            }
+        }
     }
 };
 
