4bede6aa6da07560d601b7f098f0fce6f5443f29
liubang <it.liubang@gmail.com>
2024-12-18 22:09:43 +0800

    update

--- cpp/pl/lang/common.h
+++ cpp/pl/lang/common.h
@@ -35,3 +35,13 @@
 #else
 #define PL_ALWAYS_INLINE inline
 #endif
+
+#if defined(__aarch64__)
+//  __builtin_prefetch(..., 1) turns into a prefetch into prfm pldl3keep. On
+// arm64 we want this as close to the core as possible to turn it into a
+// L1 prefetech unless locality == 0 in which case it will be turned into a
+// non-temporal prefetch
+#define PL_PREFETCH(addr, rw, locality) __builtin_prefetch(addr, rw, locality >= 1 ? 3 : locality)
+#else
+#define PLPREFETCH(addr, rw, locality) __builtin_prefetch(addr, rw, locality)
+#endif
