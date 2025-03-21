a389f6050c473fe49699c3f865ba55766d41bfd4
liubang <it.liubang@gmail.com>
2025-03-21 20:29:06 +0800

    update

---
+++ cpp/pl/sst/compression.h
@@ -0,0 +1,59 @@
+// Copyright (c) 2025 The Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Authors: liubang (it.liubang@gmail.com)
+#pragma once
+
+#include "cpp/pl/status/status.h"
+#include "cpp/pl/utility/utility.h"
+
+namespace pl {
+
+class CompressionAdapter : public DisableCopyAndMove {
+public:
+    virtual ~CompressionAdapter() = default;
+
+    virtual Status compress(std::string_view input, std::string* output) = 0;
+
+    virtual Status uncompress(std::string_view input, std::string* output) = 0;
+};
+
+class SnappyCompressionAdapter : public CompressionAdapter {
+public:
+    ~SnappyCompressionAdapter() override = default;
+
+    Status compress(std::string_view input, std::string* output) override;
+
+    Status uncompress(std::string_view input, std::string* output) override;
+};
+
+class ZstdCompressionAdapter : public CompressionAdapter {
+public:
+    ~ZstdCompressionAdapter() override = default;
+
+    Status compress(std::string_view input, std::string* output) override;
+
+    Status uncompress(std::string_view input, std::string* output) override;
+};
+
+class IsalCompressionAdapter : public CompressionAdapter {
+public:
+    ~IsalCompressionAdapter() override = default;
+
+    Status compress(std::string_view input, std::string* output) override;
+
+    Status uncompress(std::string_view input, std::string* output) override;
+};
+
+} // namespace pl
