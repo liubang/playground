b51c488c19b1340b84563babd76b171e2e848b26
liubang <it.liubang@gmail.com>
2025-06-21 23:07:32 +0800

    update

--- java/design-pattern/src/main/java/cn/iliubang/exercises/dp/factory/abstractfactory/LowTyre.java
+++
@@ -1,8 +0,0 @@
-package cn.iliubang.exercises.dp.factory.abstractfactory;
-
-public class LowTyre implements Tyre {
-    @Override
-    public void revolve() {
-        System.out.println("磨损快");
-    }
-}
