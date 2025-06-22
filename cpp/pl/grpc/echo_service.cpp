315afaeef72eb2dc89ca4b293e05565a81e701f0
liubang <it.liubang@gmail.com>
2025-06-22 10:51:28 +0800

    update

---
+++ cpp/pl/grpc/echo_service.cpp
@@ -0,0 +1,41 @@
+// Copyright (c) 2025 The Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Authors: liubang (it.liubang@gmail.com)
+
+#include "echo_service_impl.h"
+
+#include "absl/flags/parse.h"
+#include "absl/log/initialize.h"
+
+#include <grpcpp/security/server_credentials.h>
+
+void RunServer() {
+    std::string server_address("0.0.0.0:50051");
+    pl::EchoServiceImpl service;
+
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+    server->Wait();
+}
+
+int main(int argc, char* argv[]) {
+    absl::ParseCommandLine(argc, argv);
+    absl::InitializeLog();
+    RunServer();
+    return 0;
+}
