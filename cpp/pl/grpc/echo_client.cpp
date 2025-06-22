8477278f4a1fc5f7c39213ae3c4d0a54f042c773
liubang <it.liubang@gmail.com>
2025-06-23 00:29:32 +0800

    update

--- cpp/pl/grpc/echo_client.cpp
+++ cpp/pl/grpc/echo_client.cpp
@@ -30,5 +30,8 @@ int main(int argc, char* argv[]) {
     std::cout << "-------------- Echo --------------" << std::endl;
     client.Echo("hello world");
 
+    std::cout << "-------------- Chat --------------" << std::endl;
+    client.Chat();
+
     return 0;
 }
