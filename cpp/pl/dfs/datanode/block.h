89cb92d2edaaadcb7e37107a6c34e836d241cbeb
liubang <it.liubang@gmail.com>
2025-07-11 16:52:20 +0800

    update

--- cpp/pl/dfs/datanode/block.h
+++ cpp/pl/dfs/datanode/block.h
@@ -18,8 +18,12 @@
 
 #include "cpp/pl/dfs/common/common.h"
 #include "cpp/pl/dfs/common/compression.h"
+#include "cpp/pl/status/result.h"
 
 #include <cstdint>
+#include <memory>
+#include <string>
+#include <type_traits>
 
 namespace pl::dfs {
 
@@ -27,19 +31,22 @@ static constexpr uint32_t kBlockMagic = 0;
 static constexpr uint32_t kMaxChunkNum = 256;
 static constexpr uint8_t kDataVersion = 1;
 
-#pragma pack(push, r, 1)
+#pragma pack(push, 1)
 struct BlockHeader {
-    uint32_t magic = kBlockMagic;
-    uint8_t version = kDataVersion;
-    uint32_t len = 0;
-    CompressionType compress_type = CompressionType::NONE;
-    uint32_t compress_chunk = 1 * MB; // 将一个block切分成小的chunk，每个chunk的大小
-    uint64_t inode = UINT64_MAX;
-    uint64_t block_id = UINT64_MAX;
-    uint32_t block_idx = UINT32_MAX;
-    uint32_t compress_index[kMaxChunkNum];
+    uint32_t magic_code;     // magic code
+    uint32_t version;        // version
+    uint32_t len;            // block length
+    uint32_t compress_type;  // compresstion type
+    uint32_t compress_chunk; // split block to small chunk and compress each chunk individually
+    uint64_t inode;          // inode
+    uint64_t block_id;       // block_id
+    uint32_t block_idx;      // block index
+    uint32_t compress_index[kMaxChunkNum]; // each chun compressed offset
 };
-#pragma pack(pop, r)
+#pragma pack(pop)
+
+static_assert(std::is_pod<BlockHeader>::value, "BlockHeader is not POD.");
+static_assert(sizeof(BlockHeader) == 1064, "sizeof(BlockHeader) not equal 1064.");
 
 struct Block {
     uint8_t version = kDataVersion;
@@ -48,6 +55,21 @@ struct Block {
     uint32_t block_idx = UINT32_MAX;
 };
 
-class BlockManager {};
+class BlockManager {
+public:
+    BlockManager();
+
+    ~BlockManager();
+
+    pl::Result<std::string> read(uint64_t block_id, uint32_t offset, uint32_t len);
+
+    pl::Result<uint32_t> write(
+        uint64_t block_id, uint32_t offset, uint32_t len, uint64_t inode, uint32_t block_idx);
+
+private:
+};
+
+using BlockManagerPtr = std::unique_ptr<BlockManager>;
+using BlockManagerRef = std::shared_ptr<BlockManager>;
 
 } // namespace pl::dfs
