111e3ef382fdd26db0de87f1b0d73b04ce4e0f5b
liubang <it.liubang@gmail.com>
2025-07-12 11:06:06 +0800

    update

--- python/manim/manimations/hello.py
+++ python/manim/manimations/hello.py
@@ -1,28 +1,45 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from manimlib import *
-from manimlib.mobject.svg.old_tex_mobject import *
+from manim import *
+import math
 
-
-class HelloWorld(InteractiveScene):
+class HelloWorld(Scene):
     def construct(self):
-        # Add some simple geometry
+        # 添加文字
         text = Text("Hello World", font_size=72)
         text.to_edge(UP)
-        self.play(Write(text, run_time=3))
+        self.play(Write(text), run_time=3)
 
+        # 圆形
         circle = Circle()
-        circle.set_flat_stroke(True)
-        circle.always.move_to(math.cos(self.time) * RIGHT)
+        circle.set_stroke(width=4)
+        # 用 ValueTracker 和 updater 来模拟运动
+        t = ValueTracker(0)
 
+        def update_circle(mob):
+            mob.move_to(math.cos(t.get_value()) * RIGHT)
+
+        circle.add_updater(update_circle)
+
+        # 正方形跟随圆形
         square = Square()
-        square.always.move_to(circle, DOWN)
 
-        self.add(circle)
-        self.add(square)
-        self.wait(10)
+        def update_square(mob):
+            mob.move_to(circle.get_center() + DOWN)
 
-        self.add(ThreeDAxes())
+        square.add_updater(update_square)
 
+        self.add(circle, square)
 
+        # 动态运行 10 秒钟
+        self.play(t.animate.increment_value(10), run_time=10, rate_func=linear)
+
+        # 移除 updater 以便后续操作不会被干扰
+        circle.clear_updaters()
+        square.clear_updaters()
+
+        # 添加 3D 坐标轴（仅演示用，Scene 中其实不会显示出 3D 效果）
+        axes = ThreeDAxes()
+        self.add(axes)
+        self.wait()
