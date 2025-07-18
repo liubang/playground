137415c8ac5b319feccfe5918e7f273b477c2ae5
liubang <it.liubang@gmail.com>
2025-07-18 15:50:03 +0800

    update

--- python/test_crawler/test_crawler.py
+++ python/test_crawler/test_crawler.py
@@ -5,10 +5,12 @@ import asyncio
 
 from crawl4ai import AsyncWebCrawler
 
+
 async def main():
     async with AsyncWebCrawler(verbose=True) as crawler:
         result = await crawler.arun(url="https://www.baidu.com")
         print(result.markdown)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
