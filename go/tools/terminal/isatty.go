7f4cae16c6ecea6a6218c3b3b7a0d4ed6a29beb9
liubang <it.liubang@gmail.com>
2025-11-19 17:20:35 +0800

    update

---
+++ go/tools/terminal/isatty.go
@@ -0,0 +1,36 @@
+// Copyright (c) 2025 The Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Authors: liubang (it.liubang@gmail.com)
+// Created: 2025/11/19 17:20
+
+package terminal
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+// Isatty returns true if the file descriptor is a terminal.
+func Isatty(fd uintptr) bool {
+	var termios Termios
+	_, _, err := syscall.Syscall6(
+		ioctlSyscall,
+		fd,
+		uintptr(ioctlReadTermios),
+		uintptr(unsafe.Pointer(&termios)),
+		0, 0, 0,
+	)
+	return err == 0
+}
