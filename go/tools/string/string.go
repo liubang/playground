fcab5aed928dd1251b042dea7ef062735e50aaa7
liubang <it.liubang@gmail.com>
2025-11-19 17:20:35 +0800

    update

---
+++ go/tools/string/string.go
@@ -0,0 +1,34 @@
+// Copyright (c) 2025 The Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Authors: liubang (it.liubang@gmail.com)
+// Created: 2025/11/19 17:20
+
+package string
+
+import (
+	"github.com/mattn/go-runewidth"
+	"github.com/rivo/uniseg"
+)
+
+func StrPrintLenInTerm(text string) uint64 {
+	gr := uniseg.NewGraphemes(text)
+	var totalWidth uint64 = 0
+	for gr.Next() {
+		cluster := gr.Str()
+		width := runewidth.StringWidth(cluster)
+		totalWidth += uint64(width)
+	}
+	return totalWidth
+}
