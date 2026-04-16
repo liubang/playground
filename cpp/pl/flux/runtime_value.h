bbef85a8c1142797d5a6fde2d4dc83995fcd802f
liubang <it.liubang@gmail.com>
2026-04-17 00:03:56 +0800

    feat: update flux cli

--- cpp/pl/flux/runtime_value.h
+++ cpp/pl/flux/runtime_value.h
@@ -107,7 +107,8 @@ public:
     static Value table(std::string bucket,
                        std::vector<std::shared_ptr<ObjectValue>> rows,
                        std::optional<std::string> range_start = std::nullopt,
-                       std::optional<std::string> range_stop = std::nullopt);
+                       std::optional<std::string> range_stop = std::nullopt,
+                       std::optional<std::string> result_name = std::nullopt);
     static Value function(std::shared_ptr<FunctionValue> function);
 
     [[nodiscard]] Type type() const { return type_; }
@@ -166,6 +167,7 @@ struct TableValue {
     std::vector<std::shared_ptr<ObjectValue>> rows;
     std::optional<std::string> range_start;
     std::optional<std::string> range_stop;
+    std::optional<std::string> result_name;
 
     [[nodiscard]] std::string string() const;
     bool operator==(const TableValue& other) const;
