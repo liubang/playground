07d532a9e0a9ade2acf57c26e7567a170749144e
liubang <it.liubang@gmail.com>
2026-05-10 14:03:40 +0800

    Add Flux RBO pipeline skeleton

---
+++ cpp/pl/flux/optimizer/rbo.h
@@ -0,0 +1,77 @@
+// Copyright (c) 2026 The Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Authors: liubang (it.liubang@gmail.com)
+// Created: 2026/05/10 00:00
+
+#pragma once
+
+#include "absl/status/statusor.h"
+#include "cpp/pl/flux/plan/plan_node.h"
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace pl::flux::optimizer {
+
+struct RuleTrace {
+    std::string rule;
+    bool applied = false;
+    std::string detail;
+};
+
+struct RuleApplication {
+    std::shared_ptr<plan::PlanNode> plan;
+    bool applied = false;
+    std::string detail;
+};
+
+struct PlanOptimizerResult {
+    std::shared_ptr<plan::PlanNode> plan;
+    std::vector<RuleTrace> trace;
+};
+
+class Rule {
+public:
+    virtual ~Rule() = default;
+
+    [[nodiscard]] virtual std::string Name() const = 0;
+    [[nodiscard]] virtual absl::StatusOr<RuleApplication> Apply(
+        const std::shared_ptr<plan::PlanNode>& plan) const = 0;
+};
+
+class PlanOptimizer {
+public:
+    virtual ~PlanOptimizer() = default;
+
+    [[nodiscard]] virtual absl::StatusOr<PlanOptimizerResult> Optimize(
+        std::shared_ptr<plan::PlanNode> plan) const = 0;
+};
+
+class RuleBasedOptimizer final : public PlanOptimizer {
+public:
+    explicit RuleBasedOptimizer(std::vector<std::unique_ptr<Rule>> rules);
+
+    [[nodiscard]] absl::StatusOr<PlanOptimizerResult> Optimize(
+        std::shared_ptr<plan::PlanNode> plan) const override;
+
+private:
+    std::vector<std::unique_ptr<Rule>> rules_;
+};
+
+RuleBasedOptimizer DefaultRuleBasedOptimizer();
+
+std::vector<std::string> AppliedRuleNames(const PlanOptimizerResult& result);
+
+} // namespace pl::flux::optimizer
