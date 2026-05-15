744b87ea2c413f99314effec1bb08bbe0ebd6fe7
liubang <it.liubang@gmail.com>
2026-05-15 23:07:15 +0800

    Add Flux runtime profiles and observable exchange DAG

--- cpp/pl/flux/execution/physical_executor.h
+++ cpp/pl/flux/execution/physical_executor.h
@@ -58,6 +58,28 @@ struct ExecutionTask {
     std::vector<Pipeline> pipelines;
 };
 
+struct PipelineProfile {
+    std::string id;
+    std::string name;
+    std::string role;
+    std::vector<std::string> dependencies;
+    std::vector<std::string> operators;
+    size_t pages = 0;
+    size_t rows = 0;
+    bool blocked = false;
+    bool finished = false;
+    std::string error;
+};
+
+struct ExecutionProfile {
+    std::vector<PipelineProfile> pipelines;
+};
+
+struct SchedulerResult {
+    Value value;
+    ExecutionProfile profile;
+};
+
 class PhysicalPlanner {
 public:
     [[nodiscard]] absl::StatusOr<ExecutionTask> Plan(
@@ -76,13 +98,19 @@ private:
 
 class Scheduler {
 public:
+    [[nodiscard]] absl::StatusOr<SchedulerResult> RunWithProfile(ExecutionTask task) const;
     [[nodiscard]] absl::StatusOr<Value> Run(ExecutionTask task) const;
 };
 
 class PhysicalExecutor {
 public:
+    [[nodiscard]] absl::StatusOr<SchedulerResult> ExecuteWithProfile(
+        const std::shared_ptr<plan::PlanNode>& logical_plan) const;
     [[nodiscard]] absl::StatusOr<Value> Execute(
         const std::shared_ptr<plan::PlanNode>& logical_plan) const;
 };
+
+std::string FormatPipelinePlan(const std::shared_ptr<plan::PlanNode>& logical_plan);
+std::string FormatExecutionProfile(const ExecutionProfile& profile);
 
 } // namespace pl::flux::execution
