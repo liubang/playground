25be2334374f8983d7ee2c1f8e2163a5eeb297ee
liubang <it.liubang@gmail.com>
2026-05-24 15:30:17 +0800

    feat(flux): centralize semantic analysis metadata

---
+++ cpp/pl/flux/analysis/semantic_analyzer.h
@@ -0,0 +1,134 @@
+// Copyright (c) 2026 The Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Authors: liubang (it.liubang@gmail.com)
+// Created: 2026/05/24 11:04
+
+#pragma once
+
+#include <cstddef>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include "cpp/pl/flux/analysis/semantic_model.h"
+#include "cpp/pl/flux/syntax/ast.h"
+
+namespace pl::flux::analysis {
+
+enum class DiagnosticSeverity {
+    Error = 1,
+    Warning = 2,
+    Info = 3,
+    Hint = 4,
+};
+
+enum class SymbolKind {
+    Variable,
+    Function,
+    Option,
+    Builtin,
+    Import,
+    Parameter,
+    PackageBuiltin,
+};
+
+enum class ReferenceKind {
+    Identifier,
+    Callee,
+    PackageObject,
+    PackageMember,
+    WithSource,
+};
+
+struct SemanticDiagnostic {
+    std::string message;
+    SourceLocation location;
+    DiagnosticSeverity severity = DiagnosticSeverity::Warning;
+};
+
+struct Symbol {
+    size_t id = 0;
+    std::string name;
+    SymbolKind kind = SymbolKind::Variable;
+    SourceLocation location;
+    size_t scope_id = 0;
+    std::optional<std::string> import_path;
+    std::optional<std::string> builtin_package;
+    std::vector<std::string> parameters;
+    Type type = Type::Unknown();
+};
+
+struct SymbolReference {
+    std::string name;
+    SourceLocation location;
+    ReferenceKind kind = ReferenceKind::Identifier;
+    size_t scope_id = 0;
+    size_t definition_id = 0;
+    std::optional<std::string> package;
+    std::optional<std::string> member;
+    bool resolved = false;
+};
+
+struct ScopeInfo {
+    size_t id = 0;
+    size_t parent_id = 0;
+    SourceLocation location;
+    std::vector<size_t> definitions;
+};
+
+struct AnalysisResult {
+    std::vector<SemanticDiagnostic> diagnostics;
+    std::vector<Symbol> definitions;
+    std::vector<SymbolReference> references;
+    std::vector<ScopeInfo> scopes;
+    std::vector<std::string> imported_packages;
+    std::vector<ExpressionInfo> expressions;
+    std::vector<CallInfo> calls;
+    std::vector<RecordSchema> record_schemas;
+    std::vector<TableSchema> table_schemas;
+
+    const Symbol* FindDefinition(size_t id) const;
+    const Symbol* FindDefinition(std::string_view name) const;
+    const Symbol* DefinitionAt(uint32_t line, uint32_t column) const;
+    const SymbolReference* ReferenceAt(uint32_t line, uint32_t column) const;
+    const Symbol* DefinitionForSymbolAt(uint32_t line, uint32_t column) const;
+    const ExpressionInfo* ExpressionAt(uint32_t line, uint32_t column) const;
+    std::optional<Type> TypeAt(uint32_t line, uint32_t column) const;
+    const CallInfo* CallAt(uint32_t line, uint32_t column) const;
+    const TableSchema* TableSchemaAt(uint32_t line, uint32_t column) const;
+    std::string SymbolAt(uint32_t line, uint32_t column) const;
+    std::vector<const SymbolReference*> ReferencesOf(const Symbol& def) const;
+    std::vector<const SymbolReference*> ReferencesOf(std::string_view name) const;
+};
+
+class SemanticAnalyzer {
+public:
+    AnalysisResult Analyze(const File& file);
+};
+
+struct PackageAnalysisResult {
+    std::vector<AnalysisResult> files;
+    std::vector<Symbol> exports;
+    std::vector<SemanticDiagnostic> diagnostics;
+
+    const Symbol* FindExport(std::string_view name) const;
+};
+
+class PackageAnalyzer {
+public:
+    PackageAnalysisResult Analyze(const Package& package);
+};
+
+} // namespace pl::flux::analysis
---
+++ cpp/pl/flux/analysis/semantic_model.h
@@ -0,0 +1,130 @@
+// Copyright (c) 2026 The Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Authors: liubang (it.liubang@gmail.com)
+// Created: 2026/05/24 11:38
+
+#pragma once
+
+#include <memory>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "cpp/pl/flux/syntax/ast.h"
+
+namespace pl::flux::analysis {
+
+enum class TypeKind {
+    Unknown,
+    Dynamic,
+    Null,
+    Bool,
+    Int,
+    UInt,
+    Float,
+    String,
+    Time,
+    Duration,
+    Regexp,
+    Label,
+    Array,
+    Dict,
+    Record,
+    Function,
+    Stream,
+    Table,
+    Error,
+};
+
+struct Type;
+
+struct RecordFieldType {
+    std::string name;
+    std::shared_ptr<Type> type;
+};
+
+struct FunctionParamType {
+    std::string name;
+    std::shared_ptr<Type> type;
+    bool optional = false;
+    bool pipe = false;
+};
+
+struct Type {
+    TypeKind kind = TypeKind::Unknown;
+    std::string name;
+    std::vector<Type> args;
+    std::vector<RecordFieldType> fields;
+    std::vector<FunctionParamType> params;
+    bool open_record = false;
+
+    static Type Unknown();
+    static Type Dynamic();
+    static Type Error();
+    static Type Scalar(TypeKind kind);
+    static Type Array(Type element);
+    static Type Dict(Type key, Type value);
+    static Type Record(std::vector<RecordFieldType> fields, bool open = false);
+    static Type Function(std::vector<FunctionParamType> params, Type result);
+    static Type Stream(Type row);
+    static Type Table(Type row);
+
+    [[nodiscard]] bool IsUnknownLike() const;
+    [[nodiscard]] bool IsNumeric() const;
+    [[nodiscard]] bool IsBool() const;
+    [[nodiscard]] bool IsString() const;
+    [[nodiscard]] bool IsStreamLike() const;
+    [[nodiscard]] std::optional<Type> Field(std::string_view name) const;
+    [[nodiscard]] std::string ToString() const;
+};
+
+bool SameType(const Type& lhs, const Type& rhs);
+bool CanAssign(const Type& expected, const Type& actual);
+Type CommonType(const Type& lhs, const Type& rhs);
+Type ParseTypeExpression(std::string_view text);
+
+struct ExpressionInfo {
+    SourceLocation location;
+    Type type;
+    std::optional<size_t> reference_index;
+};
+
+struct CallInfo {
+    SourceLocation location;
+    SourceLocation callee_location;
+    std::string callee;
+    std::optional<std::string> package;
+    std::optional<std::string> member;
+    std::vector<std::string> argument_names;
+    Type return_type;
+    bool pipe_value_present = false;
+    bool builtin = false;
+};
+
+struct RecordSchema {
+    SourceLocation location;
+    std::vector<RecordFieldType> fields;
+    bool open = false;
+};
+
+struct TableSchema {
+    SourceLocation location;
+    std::vector<RecordFieldType> columns;
+    std::vector<std::string> group_key;
+    bool open = false;
+};
+
+} // namespace pl::flux::analysis
